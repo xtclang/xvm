ee54d1971621d23a53943c94921b0dbcc5d256b3
Cameron Purdy <cameron@xtclang.org>
2016-10-29 19:57:18 -0400

    Changes to be committed: 	modified:   src/org/xvm/asm/ClassStructure.java 	new file:   src/org/xvm/util/ListMap.java

--- src/org/xvm/asm/ClassStructure.java
+++ src/org/xvm/asm/ClassStructure.java
@@ -1,10 +1,20 @@
 package org.xvm.asm;
 
 
-import org.xvm.asm.ConstantPool.ClassConstant;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.Map;
 import org.xvm.asm.StructureContainer.MethodContainer;
+import org.xvm.asm.ConstantPool.CharStringConstant;
+import org.xvm.asm.ConstantPool.ClassConstant;
 
+import org.xvm.util.ListMap;
 
+import static org.xvm.util.Handy.readIndex;
+import static org.xvm.util.Handy.writePackedLong;
 
 
 /**
@@ -15,6 +25,8 @@ import org.xvm.asm.StructureContainer.MethodContainer;
 public class ClassStructure
         extends MethodContainer
     {
+    // ----- constructors ------------------------------------------------------
+    
     /**
      * Construct a ClassStructure with the specified identity.
      *
@@ -31,6 +43,68 @@ public class ClassStructure
         }
 
 
+    // ----- XvmStructure methods ----------------------------------------------
+
+    @Override
+    protected void disassemble(DataInput in)
+            throws IOException
+        {
+        m_constModule = (ModuleConstant) getConstantPool().getConstant(readIndex(in));
+
+        super.disassemble(in);
+        }
+
+    @Override
+    protected void registerConstants(ConstantPool pool)
+        {
+        m_constModule = (ModuleConstant) pool.register(m_constModule);
+
+        super.registerConstants(pool);
+        }
+
+    @Override
+    protected void assemble(DataOutput out)
+            throws IOException
+        {
+        writePackedLong(out, m_constModule == null ? -1 : m_constModule.getPosition());
+
+        super.assemble(out);
+        }
+
+    // TODO validate
+
+    @Override
+    public String getDescription()
+        {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.getDescription())
+                .append(", import-module=")
+                .append(m_constModule == null ? "n/a" : m_constModule);
+        return sb.toString();
+        }
+
+
+    // ----- Object methods ----------------------------------------------------
+
+    @Override
+    public boolean equals(Object obj)
+        {
+        if (obj == this)
+            {
+            return true;
+            }
+
+        if (!(obj instanceof PackageStructure && super.equals(obj)))
+            {
+            return false;
+            }
+
+        // compare imported modules
+        PackageStructure that = (PackageStructure) obj;
+        return Handy.equals(this.m_constModule, that.m_constModule);
+        }
+
+
     // ----- accessors ---------------------------------------------------------
 
     /**
@@ -44,6 +118,54 @@ public class ClassStructure
         return (ClassConstant) getIdentityConstant();
         }
 
+    public Category getCategory()
+        {
+        return m_category;
+        }
+
+    public void setCategory(Category category)
+        {
+        m_category = category;
+        markModified();
+        }
+    
+    public boolean isSingleton()
+        {
+        return m_fSingleton;
+        }
+    
+    public void setSingleton(boolean fSingleton)
+        {
+        m_fSingleton = fSingleton;
+        markModified();
+        }
+
+    public boolean isSynthetic()
+        {
+        return m_fSynthetic;
+        }
+
+    public void setSynthetic(boolean fSynthetic)
+        {
+        m_fSynthetic = fSynthetic;
+        markModified();
+        }
+
+    public Map<CharStringConstant, Constant> getTypeParams()
+        {
+        return Collections.unmodifiableMap(m_mapParams);
+        }
+
+    // TODO type parameters: index (contiguous starting with 0), name (unique), type (defaulting to "Object")
+    // public addTypeParameter(String sName, sType)
+
+
+    // ----- enumeration: class categories -------------------------------------
+
+    /**
+     * Types of classes.
+     * TODO what about "type" i.e. a class without identity?
+     */
     public enum Category
         {
         Interface,
@@ -55,12 +177,28 @@ public class ClassStructure
         Enum,
         }
 
-    private Category m_fCategory;
 
+    // ----- data members ------------------------------------------------------
+
+    /**
+     * The category of the class.
+     */
+    private Category m_category;
+
+    /**
+     * True if the class is a singleton within its module/container at runtime.
+     */
     private boolean m_fSingleton;
 
+    /**
+     * True if the class is synthetic (created by the compiler, not explicitly
+     * specified by the developer.
+     */
     private boolean m_fSynthetic;
 
-
+    /**
+     * The name-to-type information for type parameters.
+     */
+    private ListMap<CharStringConstant, ? extends Constant> m_mapParams;
     }
 
---
+++ src/org/xvm/util/ListMap.java
@@ -0,0 +1,84 @@
+package org.xvm.util;
+
+
+import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+
+/**
+ * A simple implementation of a Map on top of an ArrayList to maintain order of
+ * insertion. This map should work well for small numbers of entries, but will
+ * degrade in performance as it grows in size.
+ *
+ * @author cp 2016.10.28
+ */
+public class ListMap<K,V>
+        extends AbstractMap<K,V>
+    {
+    @Override
+    public V put(K key, V value)
+        {
+        Entry<K,V> entry = getEntry(key);
+        if (entry != null)
+            {
+            return entry.setValue(value);
+            }
+
+        m_list.add(new SimpleEntry<K, V>(key, value));
+        return null;
+        }
+
+    @Override
+    public Set<Entry<K, V>> entrySet()
+        {
+        return m_setEntries;
+        }
+
+    /**
+     * Internal: Obtain the entry that corresponds to the specified key.
+     *
+     * @param key  the key
+     *
+     * @return the entry if it exists; otherwise null
+     */
+    protected Entry<K,V> getEntry(K key)
+        {
+        for (Entry<K,V> entry : m_list)
+            {
+            if (Handy.equals(key, entry.getKey()))
+                {
+                return entry;
+                }
+            }
+
+        return null;
+        }
+
+    /**
+     * The contents of the map are stored in an ArrayList of SimpleEntry
+     * objects.
+     */
+    private final ArrayList<Entry<K, V>> m_list = new ArrayList<>();
+
+    /**
+     * The AbstractMap implementation needs an underlying "entry set" to be
+     * provided; this is that set, but just sitting on top of {@link #m_list}.
+     */
+    private final Set<Entry<K, V>> m_setEntries = new AbstractSet<Entry<K, V>>()
+        {
+        @Override
+        public Iterator<Entry<K, V>> iterator()
+            {
+            return m_list.iterator();
+            }
+
+        @Override
+        public int size()
+            {
+            return m_list.size();
+            }
+        };
+    }
