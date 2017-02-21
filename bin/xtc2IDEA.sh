c797053930896b2a58224e0a02012a96ce2d2d66
Gene Gleyzer <gene@xqiz.it>
2017-02-21 12:08:04 -0500

    Added “xsrc” directory to the project

--- bin/xtc2IDEA.sh
+++ bin/xtc2IDEA.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 # install the .x source code syntax highlighting into the IntelliJ IDEA 14
+
 binDir=$(dirname "$BASH_SOURCE")
-cp ${binDir}/Ecstasy.xml ~/Library/Preferences/IntelliJIdea14/filetypes/
+ideaDir=~/Library/Preferences/IntelliJIdea14
+
+if [ ! -d ${ideaDir} ]
+then
+  echo "***" Intellij IDEA 14 is not installed "***"
+  exit
+fi
+
+typesDir=${ideaDir}/filetypes
+
+if [ ! -d ${typesDir} ]
+then
+  mkdir ${typesDir}
+fi
+cp ${binDir}/Ecstasy.xml ${typesDir}
+
 echo "***" restart the IntelliJ "***"
