03e0e1d8d1caf6b9bb7005ff3b9b0796d162893e
Gene Gleyzer <gene@xqiz.it>
2017-02-21 09:58:33 -0500

    IntelliJ 14 syntax support for Xtc

---
+++ bin/xtc2IDEA.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install the .x source code syntax highlighting into the IntelliJ IDEA 14
+binDir=$(dirname "$BASH_SOURCE")
+cp ${binDir}/Ecstasy.xml /Users/ggleyzer/Library/Preferences/IntelliJIdea14/filetypes/
+echo "***" restart the IntelliJ "***"
