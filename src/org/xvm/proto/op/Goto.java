afc7c26c28889c405eb16e117cb75c2393e45768
Gene Gleyzer <gene@xqiz.it>
2017-03-23 17:59:28 -0400

    Changes to be committed: 	new file:   src/org/xvm/proto/op/Goto.java

---
+++ src/org/xvm/proto/op/Goto.java
@@ -0,0 +1,26 @@
+package org.xvm.proto.op;
+
+import org.xvm.proto.Frame;
+import org.xvm.proto.Op;
+
+/**
+ * GOTO op-code.
+ *
+ * @author gg 2017.03.08
+ */
+public class GoTo extends Op
+    {
+    private final int f_nAbsAddr;
+
+    public GoTo(int nAbsAddr)
+        {
+        f_nAbsAddr = nAbsAddr;
+        }
+
+
+    @Override
+    public int process(Frame frame, int iPC)
+        {
+        return f_nAbsAddr;
+        }
+    }
