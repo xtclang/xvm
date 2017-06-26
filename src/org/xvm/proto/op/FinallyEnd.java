54d727989a939b2d94bb3ff5c8cff1b264144b7b
Gene Gleyzer <gene@xqiz.it>
2017-06-26 10:22:33 -0400

    try-finally

--- src/org/xvm/proto/op/FinallyEnd.java
+++ src/org/xvm/proto/op/FinallyEnd.java
@@ -5,7 +5,7 @@ import org.xvm.proto.ObjectHandle.ExceptionHandle;
 import org.xvm.proto.Op;
 
 /**
- * END_HANDLER rel-addr ; finish an exception handler with a jump
+ * END_FINALLY ; finish a "finally" handler // note: EXIT
  *
  * @author gg 2017.03.08
  */
@@ -20,6 +20,7 @@ public class FinallyEnd extends Op
         {
         // a possible exception sits in the first variable of this scope,
         // which is the same as the "next" variable in the previous scope
+        // (see Frame.findGuard and FinallyStart.process)
         int nException = frame.f_anNextVar[frame.m_iScope - 1];
 
         ExceptionHandle hException = (ExceptionHandle) frame.f_ahVar[nException];
--- src/org/xvm/proto/op/FinallyStart.java
+++ src/org/xvm/proto/op/FinallyStart.java
@@ -4,7 +4,7 @@ import org.xvm.proto.Frame;
 import org.xvm.proto.OpInvocable;
 
 /**
- * FINALLY ; begin a "finally" handler (implicit ENTER and an exception var)
+ * FINALLY ; begin a "finally" handler (implicit EXIT/ENTER and an exception var)
  *
  * @author gg 2017.03.08
  */
@@ -21,10 +21,11 @@ public class FinallyStart extends OpInvocable
 
         int iScope = frame.enterScope();
 
-        // if coming from Frame.findGuard(), there is an exception at
-        // anNextVar[iScope] + 1
-
-        // we need to reserve the slot (unassigned) when coming in normally
+        // this op-code can only be reached by the normal flow of execution,
+        // while upon an exception, the GuardAll would jump to the very next op
+        // (called from Frame.findGuard) with an exception at anNextVar[iScope] + 1,
+        // so we need to reserve the slot (unassigned) when coming in normally;
+        // presence or absence of the exception will be checked by the FinallyEnd
         frame.f_anNextVar[iScope]++;
 
         return iPC + 1;
