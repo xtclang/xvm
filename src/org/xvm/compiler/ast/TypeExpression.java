8449a8c4f212ac2bbf59af6c1626bddd4e448d94
Cameron Purdy <cameron@xtclang.org>
2017-08-30 19:02:24 -0400

    work on constants

--- src/org/xvm/compiler/ast/TypeExpression.java
+++ src/org/xvm/compiler/ast/TypeExpression.java
@@ -1,20 +1,16 @@
 package org.xvm.compiler.ast;
 
 
-import org.xvm.asm.Constants.Access;
+import org.xvm.asm.Constant;
 
-import org.xvm.asm.constants.ParameterizedTypeConstant;
-import org.xvm.asm.constants.IdentityConstant;
 import org.xvm.asm.constants.ResolvableConstant;
 import org.xvm.asm.constants.TypeConstant;
-
-import org.xvm.compiler.ErrorListener;
 
 
 /**
  * A type expression is used to specify an abstract data type. In its compiled form, there are many
  * different possible representations of an abstract data type, depending on how it is declared, and
- * depending on how it is used. A TypeExpression may be used to indicate eitherh an IdentityConstant
+ * depending on how it is used. A TypeExpression may be used to indicate eitherh an Constant
  * (such as a ModuleConstant, PackageConstant, ClassConstant, etc.) or a TypeConstant (such as a
  * ClassTypeConstant, a ParameterTypeConstant, etc.) Often, a type expression must provide a
  * compiled representation of itself before it is able to resolve what its actual ADT will be; in
@@ -36,24 +32,33 @@ public abstract class TypeExpression
         }
 
     /**
-     * Obtain the IdentityConstant currently associated with this TypeExpression, creating an
+     * Obtain the Constant currently associated with this TypeExpression, creating an
      * UnresolvedClassConstant as a temporary place-holder if necessary. As a result of calling this
      * method, the TypeExpression becomes responsible for resolving the component that is referred
      * to by the TypeExpression; in other words, the TypeExpression becomes responsible for
-     * eventually producing a resolved IdentityConstant.
+     * eventually producing a resolved Constant.
      *
-     * @return an IdentityConstant
+     * @return an Constant
      */
-    public IdentityConstant ensureIdentityConstant()
+    public Constant ensureIdentityConstant()
         {
-        IdentityConstant constClass = getIdentityConstant();
+        Constant constClass = getIdentityConstant();
         if (constClass == null)
             {
-            constClass = new UnresolvedClassConstant(getConstantPool(), this);
+            constClass = instantiateIdentityConstant();
             setIdentityConstant(constClass);
             }
         return constClass;
         }
+
+    /**
+     * TODO new UnresolvedNameConstant(getConstantPool(), "name_goes_here");
+     *
+     * @return a constant that identifies the structure; either an IdentityConstant or a
+     *         PseudoConstant
+     */
+    protected abstract Constant instantiateIdentityConstant();
+
 
     /**
      * Obtain the TypeConstant currently associated with this TypeExpression, creating an unresolved
@@ -66,61 +71,47 @@ public abstract class TypeExpression
         TypeConstant constType = getTypeConstant();
         if (constType == null)
             {
-            constType = new UnresolvedTypeConstant(getConstantPool(), this);
+            constType = instantiateTypeConstant();
             setTypeConstant(constType);
             }
         return constType;
         }
 
     /**
-     * Create a representation of this type as an XVM ClassTypeConstant. This may produce an error.
-     * This should be overridden by any TypeExpression that has better knowledge about how to form
-     * a ClassTypeConstant, or that needs to log an error.
+     * This needs to be overridden by any TypeExpression that evaluates to something other than a
+     * class type.
      *
-     * @param errs  for logging any errors that occur attempting to represent the type as a class
-     *              type
-     *
-     * @return a ClassTypeConstant
+     * @return
      */
-    public ParameterizedTypeConstant asClassTypeConstant(ErrorListener errs)
+    protected TypeConstant instantiateTypeConstant()
         {
-        TypeConstant constType = getTypeConstant();
-        if (constType instanceof ParameterizedTypeConstant)
-            {
-            return (ParameterizedTypeConstant) constType;
-            }
-
-        return getConstantPool().ensureClassTypeConstant(ensureIdentityConstant(), Access.PUBLIC);
+        return getConstantPool().ensureClassTypeConstant(ensureIdentityConstant(), null);
         }
 
 
     // ----- accessors -----------------------------------------------------------------------------
 
     /**
-     * @return the IdentityConstant currently associated with this TypeExpression, or null if no
-     *         one has required an IdentityConstant
+     * @return the Constant that identifies the structure (pseudo or real) that this expression
+     *         refers to
      */
-    public IdentityConstant getIdentityConstant()
+    public Constant getIdentityConstant()
         {
-        IdentityConstant constId = m_constId;
-        if (constId instanceof UnresolvedClassConstant)
+        Constant constId = m_constId;
+        if (constId instanceof ResolvableConstant)
             {
-            IdentityConstant constResolved = ((UnresolvedClassConstant) constId).getResolvedConstant();
-            if (constResolved != null)
-                {
-                constId = constResolved;
-                setIdentityConstant(constId);
-                }
+            constId = ((ResolvableConstant) constId).unwrap();
+            setIdentityConstant(constId);
             }
         return constId;
         }
 
     /**
-     * @param constId  the IdentityConstant to associate with this TypeExpression
+     * @param constId  the Constant to associate with this TypeExpression
      */
-    protected void setIdentityConstant(IdentityConstant constId)
+    protected void setIdentityConstant(Constant constId)
         {
-        IdentityConstant constPrev = m_constId;
+        Constant constPrev = m_constId;
         if (constId != constPrev)
             {
             // if the previous identity constant was a resolvable constant, then resolve it to
@@ -140,17 +131,7 @@ public abstract class TypeExpression
      */
     public TypeConstant getTypeConstant()
         {
-        TypeConstant constType = m_constType;
-        if (constType instanceof UnresolvedTypeConstant)
-            {
-            TypeConstant constResolved = ((UnresolvedTypeConstant) constType).getResolvedConstant();
-            if (constResolved != null)
-                {
-                constType = constResolved;
-                setTypeConstant(constType);
-                }
-            }
-        return constType;
+        return m_constType;
         }
 
     /**
@@ -158,28 +139,17 @@ public abstract class TypeExpression
      */
     protected void setTypeConstant(TypeConstant constType)
         {
-        TypeConstant constPrev = m_constType;
-        if (constType != constPrev)
-            {
-            // if the previous type constant was a resolvable type constant, then resolve it to
-            // point to the new type constant
-            if (constPrev instanceof ResolvableConstant)
-                {
-                ((ResolvableConstant) constPrev).resolve(constType);
-                }
-
-            // store the new type constant
-            m_constType = constType;
-            }
+        // store the new type constant
+        m_constType = constType;
         }
 
 
     // ----- fields --------------------------------------------------------------------------------
 
     /**
-     * The IdentityConstant identifying the class of this TypeExpression.
+     * The Constant identifying the class of this TypeExpression.
      */
-    private IdentityConstant m_constId;
+    private Constant m_constId;
 
     /**
      * The TypeConstant currently associated with this TypeExpression.
