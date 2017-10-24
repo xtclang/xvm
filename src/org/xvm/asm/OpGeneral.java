a209f026bd8e4c655003b629f0396f7adcd0ad44
Gene Gleyzer <gene@xqiz.it>
2017-10-24 17:45:22 -0400

    Allowed local property for GP_*

---
+++ src/org/xvm/asm/OpGeneral.java
@@ -0,0 +1,210 @@
+package org.xvm.asm;
+
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.xvm.runtime.Frame;
+import org.xvm.runtime.ObjectHandle;
+import org.xvm.runtime.ObjectHandle.ExceptionHandle;
+import org.xvm.runtime.Utils;
+
+import static org.xvm.util.Handy.readPackedInt;
+import static org.xvm.util.Handy.writePackedLong;
+
+
+/**
+ * Base class for GP_ op codes.
+ */
+public abstract class OpGeneral
+        extends Op
+    {
+    /**
+     * Construct an unary op for the passed arguments.
+     *
+     * @param argTarget  the target Argument
+     * @param argReturn  the Argument to move the result into
+     */
+    protected OpGeneral(Argument argTarget, Argument argReturn)
+        {
+        assert(!isBinaryOp());
+
+        m_argTarget = argTarget;
+        m_argReturn = argReturn;
+        }
+
+    /**
+     * Construct an binary op for the passed arguments.
+     *
+     * @param argTarget  the target Argument
+     * @param argValue   the second value Argument
+     * @param argReturn  the Argument to store the result into
+     */
+    protected OpGeneral(Argument argTarget, Argument argValue, Argument argReturn)
+        {
+        assert(isBinaryOp());
+
+        m_argTarget = argTarget;
+        m_argValue  = argValue;
+        m_argReturn = argReturn;
+        }
+
+    /**
+     * Deserialization constructor.
+     *
+     * @param in      the DataInput to read from
+     * @param aconst  an array of constants used within the method
+     */
+    protected OpGeneral(DataInput in, Constant[] aconst)
+            throws IOException
+        {
+        m_nTarget = readPackedInt(in);
+        if (isBinaryOp())
+            {
+            m_nArgValue = readPackedInt(in);
+            }
+        m_nRetValue = readPackedInt(in);
+        }
+
+    @Override
+    public void write(DataOutput out, ConstantRegistry registry)
+            throws IOException
+        {
+        if (m_argTarget != null)
+            {
+            m_nTarget = encodeArgument(m_argTarget, registry);
+            if (isBinaryOp())
+                {
+                m_nArgValue = encodeArgument(m_argValue,  registry);
+                }
+            m_nRetValue = encodeArgument(m_argReturn, registry);
+            }
+
+        out.writeByte(OP_GP_ADD);
+        writePackedLong(out, m_nTarget);
+        if (isBinaryOp())
+            {
+            writePackedLong(out, m_nArgValue);
+            }
+        writePackedLong(out, m_nRetValue);
+        }
+
+    /**
+     * A "virtual constant" indicating whether or not this op is a binary one (has two arguments).
+     *
+     * @return true iff the op has two arguments
+     */
+    protected boolean isBinaryOp()
+        {
+        // majority of the ops are binary; let's default to that
+        return true;
+        }
+
+    @Override
+    public int process(Frame frame, int iPC)
+        {
+        return isBinaryOp() ? processBinaryOp(frame) : processUnaryOp(frame);
+        }
+
+    protected int processUnaryOp(Frame frame)
+        {
+        try
+            {
+            ObjectHandle hTarget = frame.getArgument(m_nTarget);
+            if (hTarget == null)
+                {
+                return R_REPEAT;
+                }
+
+            if (isProperty(hTarget))
+                {
+                ObjectHandle[] ahValue = new ObjectHandle[] {hTarget};
+                Frame.Continuation stepNext = frameCaller ->
+                    completeUnary(frameCaller, ahValue[0]);
+
+                return new Utils.GetArgument(ahValue, stepNext).doNext(frame);
+                }
+
+            return completeUnary(frame, hTarget);
+            }
+        catch (ExceptionHandle.WrapperException e)
+            {
+            return frame.raiseException(e);
+            }
+        }
+
+    protected int completeUnary(Frame frame, ObjectHandle hTarget)
+        {
+        if (frame.isNextRegister(m_nRetValue))
+            {
+            frame.introduceVarCopy(m_nTarget);
+            }
+        return Op.R_NEXT;
+        }
+
+    protected int processBinaryOp(Frame frame)
+        {
+        try
+            {
+            ObjectHandle hTarget = frame.getArgument(m_nTarget);
+            ObjectHandle hArg = frame.getArgument(m_nArgValue);
+            if (hTarget == null || hArg == null)
+                {
+                return R_REPEAT;
+                }
+
+            if (isProperty(hTarget) || isProperty(hArg))
+                {
+                ObjectHandle[] ahValue = new ObjectHandle[] {hTarget, hArg};
+                Frame.Continuation stepNext = frameCaller ->
+                    completeBinary(frameCaller, ahValue[0], ahValue[1]);
+
+                return new Utils.GetArguments(ahValue, stepNext).doNext(frame);
+                }
+
+            return completeBinary(frame, hTarget, hArg);
+            }
+        catch (ExceptionHandle.WrapperException e)
+            {
+            return frame.raiseException(e);
+            }
+        }
+
+    protected int completeBinary(Frame frame, ObjectHandle hTarget, ObjectHandle hArg)
+        {
+        if (frame.isNextRegister(m_nRetValue))
+            {
+            frame.introduceVarCopy(m_nTarget);
+            }
+        return Op.R_NEXT;
+        }
+
+    @Override
+    public void simulate(Scope scope)
+        {
+        if (scope.isNextRegister(m_nRetValue))
+            {
+            scope.allocVar();
+            }
+        }
+
+    @Override
+    public void registerConstants(ConstantRegistry registry)
+        {
+        registerArgument(m_argTarget, registry);
+        if (isBinaryOp())
+            {
+            registerArgument(m_argValue, registry);
+            }
+        registerArgument(m_argReturn, registry);
+        }
+
+    protected int m_nTarget;
+    protected int m_nArgValue;
+    protected int m_nRetValue;
+
+    private Argument m_argTarget;
+    private Argument m_argValue;
+    private Argument m_argReturn;
+    }
--- src/org/xvm/asm/op/GP_Neg.java
+++ src/org/xvm/asm/op/GP_Neg.java
@@ -2,28 +2,20 @@ package org.xvm.asm.op;
 
 
 import java.io.DataInput;
-import java.io.DataOutput;
 import java.io.IOException;
 
 import org.xvm.asm.Constant;
-import org.xvm.asm.Op;
-import org.xvm.asm.Register;
-import org.xvm.asm.Scope;
+import org.xvm.asm.OpGeneral;
 
 import org.xvm.runtime.Frame;
 import org.xvm.runtime.ObjectHandle;
-import org.xvm.runtime.ObjectHandle.ExceptionHandle;
-import org.xvm.runtime.Utils;
-
-import static org.xvm.util.Handy.readPackedInt;
-import static org.xvm.util.Handy.writePackedLong;
 
 
 /**
  * GP_NEG rvalue, lvalue   ; -T -> T
  */
 public class GP_Neg
-        extends Op
+        extends OpGeneral
     {
     /**
      * Construct a GP_NEG op.
@@ -35,6 +27,8 @@ public class GP_Neg
      */
     public GP_Neg(int nArg, int nRet)
         {
+        super((Argument) null, null);
+
         m_nArgValue = nArg;
         m_nRetValue = nRet;
         }
@@ -43,16 +37,11 @@ public class GP_Neg
      * Construct a GP_NEG op for the passed arguments.
      *
      * @param argValue  the Argument to negate
-     * @param regResult  the Register to put the result in
+     * @param argResult  the Argument to store the result in
      */
-    public GP_Neg(Argument argValue, Register regResult)
+    public GP_Neg(Argument argValue, Argument argResult)
         {
-        if (argValue == null || regResult == null)
-            {
-            throw new IllegalArgumentException("arguments required");
-            }
-        m_argValue = argValue;
-        m_regResult = regResult;
+        super(argValue, argResult);
         }
 
     /**
@@ -64,24 +53,7 @@ public class GP_Neg
     public GP_Neg(DataInput in, Constant[] aconst)
             throws IOException
         {
-        m_nArgValue = readPackedInt(in);
-        m_nRetValue = readPackedInt(in);
-        }
-
-    @Override
-    public void write(DataOutput out, ConstantRegistry registry)
-            throws IOException
-        {
-        if (m_argValue != null)
-            {
-            m_nArgValue = encodeArgument(m_argValue, registry);
-            m_nRetValue = encodeArgument(m_regResult, registry);
-            }
-
-        out.writeByte(OP_GP_NEG);
-
-        writePackedLong(out, m_nArgValue);
-        writePackedLong(out, m_nRetValue);
+        super(in, aconst);
         }
 
     @Override
@@ -91,60 +63,10 @@ public class GP_Neg
         }
 
     @Override
-    public int process(Frame frame, int iPC)
+    protected int completeUnary(Frame frame, ObjectHandle hTarget)
         {
-        try
-            {
-            ObjectHandle hTarget = frame.getArgument(m_nArgValue);
-            if (hTarget == null)
-                {
-                return R_REPEAT;
-                }
+        super.completeUnary(frame, hTarget);
 
-            if (isProperty(hTarget))
-                {
-                ObjectHandle[] ahTarget = new ObjectHandle[] {hTarget};
-                Frame.Continuation stepNext = frameCaller -> complete(frameCaller, ahTarget[0]);
-
-                return new Utils.GetArgument(ahTarget, stepNext).doNext(frame);
-                }
-
-            return complete(frame, hTarget);
-            }
-        catch (ExceptionHandle.WrapperException e)
-            {
-            return frame.raiseException(e);
-            }
-        }
-
-    protected int complete(Frame frame, ObjectHandle hTarget)
-        {
-        if (frame.isNextRegister(m_nRetValue))
-            {
-            frame.introduceVarCopy(m_nArgValue);
-            }
         return hTarget.f_clazz.f_template.invokeNeg(frame, hTarget, m_nRetValue);
         }
-
-    @Override
-    public void simulate(Scope scope)
-        {
-        if (scope.isNextRegister(m_nRetValue))
-            {
-            scope.allocVar();
-            }
-        }
-
-    @Override
-    public void registerConstants(ConstantRegistry registry)
-        {
-        registerArgument(m_argValue, registry);
-        }
-
-    private int m_nArgValue;
-    private int m_nRetValue;
-
-    private Argument m_argValue;
-    private Register m_regResult;
-
     }
