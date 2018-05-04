c231f6e7f970b914918d4f85036021ff92a12530
Cameron Purdy <cameron@xtclang.org>
2018-05-04 09:50:42 -0400

    wip name resolution

--- src/org/xvm/compiler/ast/InvocationExpression.java
+++ src/org/xvm/compiler/ast/InvocationExpression.java
@@ -24,9 +24,106 @@ import org.xvm.util.Severity;
 
 
 /**
- * Invocation expression represents calling a method or function.
+ * Invocation expression represents calling a method or function. An oversimplification of the
+ * model is as follows:
+ * <ul>
+ * <li><i>"Binding a method"</i>: Reference + Method = Function</li>
+ * <li><i>"Binding parameters" (aka currying)</i>: Function + Argument(s) = Function'</li>
+ * <li><i>"Calling a function"</i>: Function + () = Return Value(s)</li>
+ * </ul>
+ * <p/>
+ * Most of the time, this is all accomplished in a single syntactic step, but not always:
+ * <p/>
+ * <pre><code>
+ *   // bind target "list" to method "add", bind argument, call function
+ *   list.add(item);
  *
- * If you already have an expression "expr", this is for "expr(args)".
+ *   // on "List" type, find "add" method with one parameter (four alternatives shown)
+ *   Method m = List.&add(?);
+ *   Method m = List.&add(&lt;List.ElementType&gt;?);
+ *   Method m = List.add(?);
+ *   Method m = List.add(&lt;List.ElementType&gt;?);
+ *
+ *    // bind target "list" to method "add", bind argument
+ *   function void () fn = list.&add(item);
+ *
+ *   // call the function held in "fn"
+ *   fn();
+ * </code></pre>
+ * <p/>
+ * There are op codes for:
+ * <ul>
+ * <li>Binding a method to its target reference to create a function;</li>
+ * <li>Binding any subset (including all) parameters of a function to create a new function;</li>
+ * <li>Calling a function (16 different ops, including short forms for calling functions whose
+ *     parameters have been bound vs. functions that still have unbound parameters);</li>
+ * <li>Invoking a method using a target reference (16 different ops);</li>
+ * <li>Instantiating a new object and invoking its constructor (16 different ops); and</li>
+ * <li>Invoking another constructor from within a constructor (4 different ops);</li>
+ * </ul>
+ * <p/>
+ * Each of these operations is type safe, requiring a provably correct target reference, arguments,
+ * and destinations for each of the return values.
+ * <p/>
+ * <pre><code>
+ *                                            bind    bind
+ *   description                              target  args    call
+ *   ---------------------------------------  ------  ------  ------
+ *   obtain reference to method or function   N       N       N
+ *   function invocation                      N       N       Y
+ *   binding function parameters / currying   N       Y       N
+ *   function invocation                      N       Y       Y
+ *   method binding                           Y       N       N
+ *   method invocation                        Y       N       Y
+ *   method and parameter binding             Y       Y       N
+ *   method invocation                        Y       Y       Y
+ * </code></pre>
+ * <p/>
+ * The implementation is specialized when the method or function <b>name</b> is provided. The
+ * invocation expression knows this situation exists because its {@link #expr} refers to a {@link
+ * NameExpression}. The responsibilities of the InvocationExpression are expanded as follows:
+ * <ul>
+ * <li>The {@link #expr} itself is <b>NOT</b> asked to validate! All of the information that it
+ *     contains is instead validated by and used by the InvocationExpression directly.</li>
+ * <li>The NameExpression's own {@link NameExpression#left left} expression (if any) represents
+ *     the class/type within which -- or reference on which -- the method or function will be
+ *     found; a lack of a left expression implies a possible "this." for non-static code contexts,
+ *     and the current name-resolution {@link Context} for both non-static and static code
+ *     contexts.</li>
+ * <li>The NameExpression's purpose in this case is to provide information to the
+ *     InvocationExpression so that it can locate the correct method or function. In addition to
+ *     the context and the name, there are optional "redundant returns" on the NameExpression in
+ *     {@link NameExpression#params params}. The InvocationExpression must validate these, and for
+ *     each redundant return type provided, it must ensure that any method/function that it selects
+ *     matches that redundant return.</li>
+ * <li>Lastly, the NameExpression includes a no-de-reference indicator, {@link
+ *     NameExpression#isSuppressDeref() isSuppressDeref()}, which tells the InvocationExpression
+ *     not to perform the "call" portion itself, but rather to yield the method or function
+ *     reference as a result. This information may overlap with information that the
+ *     InvocationExpression has from its own method arguments, if any is a NonBindingExpression,
+ *     since that also indicates that the InvocationExpression must not to perform the "call", but
+ *     rather yields a method or function reference as its result.</li>
+ * <li>The rules for determining th</li>
+ * </ul>
+ * <p/>
+ * The rules for determining the method or function to call when the name is provided:
+ * <ul>
+ * <li>First, validate the (optional) left expression, and all of the (optional) params expressions
+ *     of the NameExpression.</li>
+ * <li>Second, determine whether the search will involve methods, functions, or both. Functions are
+ *     included if (i) there is no left, or (ii) the left is identity-mode. Methods are included if
+ *     (i) there is a left, or (ii) there is no left and the context is not static.</li>
+ * <li>If there is no left, look for functions and methods that exist as children of the current
+ *     component (which may be the method TODO), walking up the component tree until the method is reached.
+ *     walking up the
+ * on the TypeInfo for the type possible methods and/or functions that </li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ *
+ * </ul>
+ * <p/>
  */
 public class InvocationExpression
         extends Expression
@@ -109,15 +206,6 @@ public class InvocationExpression
         return TypeConstant.NO_TYPES;
         }
 
-
-    // TODO getValueCount() - could be any #?
-
-    @Override
-    public boolean isConstant()
-        {
-        // assume all invocations can have side effects
-        return false;
-        }
 
     @Override
     protected Expression validate(Context ctx, TypeConstant typeRequired, TuplePref pref, ErrorListener errs)
@@ -291,6 +379,19 @@ public class InvocationExpression
     protected Expression       expr;
     protected List<Expression> args;
     protected long             lEndPos;
+
+    // bind target: F/T
+    // bind parameters: F/T
+    // call: F/T
+    //
+
+    private transient boolean fBindTarget;
+    private transient boolean fBindParams;
+    private transient boolean fCall;
+
+    enum BindParams {None, Partial, All}
+    enum CallPlan {None, Invoke, Call}
+
 
     private static final Field[] CHILD_FIELDS = fieldsForNames(InvocationExpression.class, "expr", "args");
     }
