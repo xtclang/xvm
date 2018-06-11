93d4e488f9d6bbe77e90a9206616b4de47948787
Cameron Purdy <cameron@xtclang.org>
2018-06-11 11:48:30 -0400

    implementing array access

--- src/org/xvm/compiler/ast/ArrayAccessExpression.java
+++ src/org/xvm/compiler/ast/ArrayAccessExpression.java
@@ -4,6 +4,14 @@ package org.xvm.compiler.ast;
 import java.lang.reflect.Field;
 
 import java.util.List;
+import java.util.Set;
+import org.xvm.asm.Argument;
+import org.xvm.asm.ErrorListener;
+import org.xvm.asm.MethodStructure.Code;
+import org.xvm.asm.constants.MethodConstant;
+import org.xvm.asm.constants.TypeConstant;
+import org.xvm.asm.constants.TypeInfo;
+import org.xvm.compiler.ast.Statement.Context;
 
 
 /**
@@ -52,9 +60,82 @@ public class ArrayAccessExpression
     // ----- compilation ---------------------------------------------------------------------------
 
     @Override
-    public boolean isConstant()
+    public TypeConstant getImplicitType(Context ctx)
         {
-        return false;
+        TypeConstant typeArray = expr.getImplicitType(ctx);
+        if (typeArray == null)
+            {
+            return null;
+            }
+
+        TypeInfo            infoArray  = typeArray.ensureTypeInfo();
+        int                 cIndexes   = indexes.size(); // REVIEW GG - what about supporting a tuple of indexes? (low priority)
+        Set<MethodConstant> setMethods = infoArray.findOpMethods("getElement", "[]", cIndexes);
+        for (MethodConstant idMethod : setMethods)
+            {
+            TypeConstant[] atypeRet = idMethod.getRawReturns();
+            if (atypeRet.length > 0)
+                {
+                return atypeRet[0];
+                }
+            }
+
+        return null;
+        }
+
+    @Override
+    protected Expression validate(Context ctx, TypeConstant typeRequired, ErrorListener errs)
+        {
+        // TODO validate expr, figure out get/set (make sure at most one of each, and at least one between them, and verify that types match)
+        m_idGet = null;
+        m_idSet = null;
+        // TODO validate indexes based on type implied by get/set methods
+        // TODO constant iff the expression and index(es?) are constant
+
+        return super.validate(ctx, typeRequired, errs);
+        }
+
+    @Override
+    public boolean isAssignable()
+        {
+        assert isValidated();
+        return m_idSet != null;
+        }
+
+    @Override
+    public Argument generateArgument(Code code, boolean fLocalPropOk, boolean fUsedOnce,
+            ErrorListener errs)
+        {
+        return super.generateArgument(code, fLocalPropOk, fUsedOnce, errs);
+        }
+
+    @Override
+    public void generateAssignment(Code code, Assignable LVal, ErrorListener errs)
+        {
+        super.generateAssignment(code, LVal, errs);
+        }
+
+    @Override
+    public Assignable generateAssignable(Code code, ErrorListener errs)
+        {
+        Argument         argArray  = expr.generateArgument(code, false, true, errs); // REVIEW local prop mode?
+        List<Expression> listIndex = indexes;
+        int              cIndexes  = listIndex.size();
+        if (cIndexes == 1)
+            {
+            Argument argIndex = listIndex.get(0).generateArgument(code, false, true, errs); // REVIEW local prop mode?
+            return new Assignable(argArray, argIndex);
+            }
+        else
+            {
+            Argument[] aArgIndexes = new Argument[cIndexes];
+            for (int i = 0; i < cIndexes; ++i)
+                {
+                aArgIndexes[i] = listIndex.get(i).generateArgument(code, false, true, errs); // REVIEW local prop mode?
+                }
+            }
+        new Assignable()
+        return super.generateAssignable(code, errs);
         }
 
 
@@ -99,6 +180,9 @@ public class ArrayAccessExpression
     protected Expression       expr;
     protected List<Expression> indexes;
     protected long             lEndPos;
+
+    private transient MethodConstant m_idGet;
+    private transient MethodConstant m_idSet;
 
     private static final Field[] CHILD_FIELDS = fieldsForNames(ArrayAccessExpression.class, "expr", "indexes");
     }
