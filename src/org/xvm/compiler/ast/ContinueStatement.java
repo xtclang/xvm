d739eb966f7566e33c072a9edef3408f9fbfa71f
Cameron Purdy <cameron@xtclang.org>
2018-10-05 14:47:37 -0400

    continue statement

--- src/org/xvm/compiler/ast/ContinueStatement.java
+++ src/org/xvm/compiler/ast/ContinueStatement.java
@@ -1,7 +1,15 @@
 package org.xvm.compiler.ast;
 
 
+import org.xvm.asm.ErrorListener;
+import org.xvm.asm.MethodStructure.Code;
+
+import org.xvm.asm.op.Jump;
+
+import org.xvm.compiler.Compiler;
 import org.xvm.compiler.Token;
+
+import org.xvm.util.Severity;
 
 
 /**
@@ -18,9 +26,39 @@ public class ContinueStatement
         }
 
 
-    // ----- accessors -----------------------------------------------------------------------------
+    // ----- compilation ---------------------------------------------------------------------------
 
+    @Override
+    protected Statement validateImpl(Context ctx, ErrorListener errs)
+        {
+        Statement stmtTarget = getTargetStatement();
+        if (stmtTarget == null)
+            {
+            if (isLabeled())
+                {
+                log(errs, Severity.ERROR, org.xvm.compiler.Compiler.MISSING_GROUND_LABEL, getLabeledName());
+                }
+            else
+                {
+                log(errs, Severity.ERROR, Compiler.MISSING_GROUND_STATEMENT);
+                }
+            return null;
+            }
+        else if (!stmtTarget.isNaturalShortCircuitStatementTarget())
+            {
+            log(errs, Severity.ERROR, Compiler.ILLEGAL_CONTINUE_TARGET);
+            return null;
+            }
 
-    // ----- fields --------------------------------------------------------------------------------
+        setJumpLabel(stmtTarget.ensureContinueLabel(ctx));
 
+        return this;
+        }
+
+    @Override
+    protected boolean emit(Context ctx, boolean fReachable, Code code, ErrorListener errs)
+        {
+        code.add(new Jump(getJumpLabel()));
+        return false;
+        }
     }
