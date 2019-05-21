90b3037c6f385970ace3a77867da7dc7e3c317ee
Gene Gleyzer <gene@xqiz.it>
2019-05-21 15:46:16 -0400

    Allow Object to Null comparison

--- src/org/xvm/compiler/ast/CmpExpression.java
+++ src/org/xvm/compiler/ast/CmpExpression.java
@@ -282,7 +282,7 @@ public class CmpExpression
         TypeConstant typeTrue   = null;
         TypeConstant typeFalse  = null;
 
-        if (!typeTarget.isNullable())
+        if (!typeTarget.isNullable() && !typeNull.isA(typeTarget))
             {
             log(errs, Severity.ERROR, Compiler.EXPRESSION_NOT_NULLABLE, typeTarget.getValueString());
             return false;
