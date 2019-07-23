36ccf5001b0eff7a63288b5b35e42f7c0a9ef6c6
Gene Gleyzer <gene@xqiz.it>
2019-07-23 14:17:59 -0400

    Make trivial refactoring

--- src/org/xvm/runtime/template/xUnsignedConstrainedInt.java
+++ src/org/xvm/runtime/template/xUnsignedConstrainedInt.java
@@ -5,6 +5,7 @@ import org.xvm.asm.ClassStructure;
 import org.xvm.runtime.ClassComposition;
 import org.xvm.runtime.Frame;
 import org.xvm.runtime.ObjectHandle;
+import org.xvm.runtime.ObjectHandle.JavaLong;
 import org.xvm.runtime.TemplateRegistry;
 
 
@@ -24,8 +25,8 @@ public abstract class xUnsignedConstrainedInt
     @Override
     public int invokeAdd(Frame frame, ObjectHandle hTarget, ObjectHandle hArg, int iReturn)
         {
-        long l1 = ((ObjectHandle.JavaLong) hTarget).getValue();
-        long l2 = ((ObjectHandle.JavaLong) hArg).getValue();
+        long l1 = ((JavaLong) hTarget).getValue();
+        long l2 = ((JavaLong) hArg).getValue();
         long lr = l1 + l2;
 
         if (f_fChecked && (((l1 & l2) | ((l1 | l2) & ~lr)) << f_cAddCheckShift) < 0)
@@ -39,8 +40,8 @@ public abstract class xUnsignedConstrainedInt
     @Override
     public int invokeSub(Frame frame, ObjectHandle hTarget, ObjectHandle hArg, int iReturn)
         {
-        long l1 = ((ObjectHandle.JavaLong) hTarget).getValue();
-        long l2 = ((ObjectHandle.JavaLong) hArg).getValue();
+        long l1 = ((JavaLong) hTarget).getValue();
+        long l2 = ((JavaLong) hArg).getValue();
         long lr = l1 - l2;
 
         if (f_fChecked && (((~l1 & l2) | ((~l1 | l2) & lr)) << f_cAddCheckShift) < 0)
@@ -55,8 +56,8 @@ public abstract class xUnsignedConstrainedInt
     public int callCompare(Frame frame, ClassComposition clazz,
                            ObjectHandle hValue1, ObjectHandle hValue2, int iReturn)
         {
-        ObjectHandle.JavaLong h1 = (ObjectHandle.JavaLong) hValue1;
-        ObjectHandle.JavaLong h2 = (ObjectHandle.JavaLong) hValue2;
+        JavaLong h1 = (JavaLong) hValue1;
+        JavaLong h2 = (JavaLong) hValue2;
 
         return frame.assignValue(iReturn, xOrdered.makeHandle(
             Long.compareUnsigned(h1.getValue(), h2.getValue())));
