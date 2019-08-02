efb0048258daee793e9fc322d85eca1cf82ad48e
Gene Gleyzer <gene@xqiz.it>
2019-08-02 16:09:12 -0400

    Fix the test script

--- bin/runAll.sh
+++ bin/runAll.sh
@@ -9,24 +9,24 @@ if [ "$1" = "-f" ]; then
 fi
 
 java -classpath ../classes/ org.xvm.runtime.TestConnector \
-    TestArray.xqiz.it tests/testTemp/array.x \
-    TestDefAsn.xqiz.it tests/testTemp/defasn.x \
-    TestTry.xqiz.it tests/testTemp/exceptions.x \
-    TestGenerics.xqiz.it tests/testTemp/generics.x \
-    TestInnerOuter.xqiz.it tests/TestTemp/innerOuter.x \
-    TestFiles.xqiz.it tests/TestTemp/files.x \
-    TestLambda.xqiz.it tests/testTemp/lambda.x \
-    TestLiterals.xqiz.it tests/TestTemp/literals.x \
-    TestLoops.xqiz.it tests/testTemp/loop.x \
-    TestNesting.xqiz.it tests/testTemp/nesting.x \
-    TestNumbers.xqiz.it tests/testTemp/numbers.x \
-    TestProps.xqiz.it tests/testTemp/prop.x \
-    TestMaps.xqiz.it tests/testTemp/maps.x \
-    TestMisc.xqiz.it tests/testTemp/misc.x \
-    TestQueues.xqiz.it tests/testTemp/queues.x \
-    TestServices.xqiz.it tests/testTemp/services.x \
-    TestReflection.xqiz.it tests/testTemp/reflect.x \
-    TestTuples.xqiz.it tests/testTemp/tuple.x
+    TestArray.xqiz.it tests/manual/array.x \
+    TestDefAsn.xqiz.it tests/manual/defasn.x \
+    TestTry.xqiz.it tests/manual/exceptions.x \
+    TestGenerics.xqiz.it tests/manual/generics.x \
+    TestInnerOuter.xqiz.it tests/manual/innerOuter.x \
+    TestFiles.xqiz.it tests/manual/files.x \
+    TestLambda.xqiz.it tests/manual/lambda.x \
+    TestLiterals.xqiz.it tests/manual/literals.x \
+    TestLoops.xqiz.it tests/manual/loop.x \
+    TestNesting.xqiz.it tests/manual/nesting.x \
+    TestNumbers.xqiz.it tests/manual/numbers.x \
+    TestProps.xqiz.it tests/manual/prop.x \
+    TestMaps.xqiz.it tests/manual/maps.x \
+    TestMisc.xqiz.it tests/manual/misc.x \
+    TestQueues.xqiz.it tests/manual/queues.x \
+    TestServices.xqiz.it tests/manual/services.x \
+    TestReflection.xqiz.it tests/manual/reflect.x \
+    TestTuples.xqiz.it tests/manual/tuple.x
 
 END_TIME=$(date +%s)
 echo "Elapsed $(($END_TIME - $START_TIME)) seconds" 1>&2
