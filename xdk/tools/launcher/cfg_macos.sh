a86ee7d2f308d445a9b90f01b6443f61c1268fd8
Gene Gleyzer <gene@xqiz.it>
2020-04-22 10:49:07 -0400

    Fix the shell script

--- xdk/tools/launcher/cfg_macos.sh
+++ xdk/tools/launcher/cfg_macos.sh
@@ -6,22 +6,22 @@ if [[ -e "$0" ]]; then
   EXP="N"
 fi
 
-if [[ ! -e "$DIR/cfg_macos.sh" ]]; then
+if [[ ! -e "${DIR}/cfg_macos.sh" ]]; then
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   EXP="Y"
 fi
 
-if [[ -e "$DIR/cfg_macos.sh" ]]; then
+if [[ -e "${DIR}/cfg_macos.sh" ]]; then
   # copy launcher to various command names
   echo "Creating command line tools: \"xtc\", \"xec\", \"xam\""
-  cp $DIR/macos_launcher $DIR/xtc
-  cp $DIR/macos_launcher $DIR/xec
-  cp $DIR/macos_launcher $DIR/xam
+  cp "${DIR}/macos_launcher" "${DIR}/xtc"
+  cp "${DIR}/macos_launcher" "${DIR}/xec"
+  cp "${DIR}/macos_launcher" "${DIR}/xam"
 
   if [[ "$EXP" == "Y" ]]; then
-    case ":${PATH:=$DIR}:" in
-        *:"$DIR":*)  ;;
-        *) export PATH="${PATH:+${PATH}:}$DIR"  ;;
+    case ":${PATH:=${DIR}}:" in
+        *:"${DIR}":*)  ;;
+        *) export PATH="${PATH:+${PATH}:}${DIR}"  ;;
     esac
   else
     echo "Unable to export PATH; use \".\" or \"source\" to execute this shell file"
