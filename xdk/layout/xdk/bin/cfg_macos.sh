2fc39622199fc994026a2531a1848a3f1d654592
Cameron Purdy <699204+cpurdy@users.noreply.github.com>
2020-04-24 13:33:25 -0400

    Add Java version check

--- xdk/layout/xdk/bin/cfg_macos.sh
+++ xdk/layout/xdk/bin/cfg_macos.sh
@@ -11,6 +11,27 @@ if [[ ! -e "${DIR}/cfg_macos.sh" ]]; then
   EXP="Y"
 fi
 
+# find the Java executable
+JTYPE=`type -p java`
+if [[ -n "${JTYPE}" ]]; then
+  JEXEC="java"
+  JADD=""
+elif [[ -n "${JAVA_HOME}" ]] && [[ -x "${JAVA_HOME}/bin/java" ]];  then
+  ADD="${JAVA_HOME}/bin/"
+  JEXEC="${ADD}java"
+else
+  echo "Unable to find the java executable; add it to PATH, or set JAVA_HOME"
+  exit 1
+fi
+
+# verify Java version 11 or later
+JVER=$("${JEXEC}" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+JMAJ="${JVER%%.*}"
+if [[ "${JMAJ}" < "11" ]]; then
+  echo "Java version is ${JVER}; Java 11 or later required"
+  exit 1
+fi
+
 if [[ -e "${DIR}/cfg_macos.sh" ]]; then
   # copy launcher to various command names
   echo "Creating command line tools: \"xtc\", \"xec\", \"xam\""
@@ -18,10 +39,14 @@ if [[ -e "${DIR}/cfg_macos.sh" ]]; then
   cp "${DIR}/macos_launcher" "${DIR}/xec"
   cp "${DIR}/macos_launcher" "${DIR}/xam"
 
-  if [[ "$EXP" == "Y" ]]; then
+  if [[ "${EXP}" == "Y" ]]; then
+    if [[ -n "${ADD}" ]]; then
+      echo "Adding Java to PATH"
+      PATH="${PATH:+${PATH}:}${ADD}"
+    fi
     case ":${PATH:=${DIR}}:" in
-        *:"${DIR}":*)  ;;
-        *) export PATH="${PATH:+${PATH}:}${DIR}"  ;;
+      *:"${DIR}":*)  ;;
+      *) export PATH="${PATH:+${PATH}:}${DIR}"  ;;
     esac
   else
     echo "Unable to export PATH; use \".\" or \"source\" to execute this shell file"
