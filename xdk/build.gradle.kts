204a09bd007f6a49d28f28c2a8ba3237c4d2edbf
Gene Gleyzer <gene@xqiz.it>
2020-06-25 18:02:28 -0400

    Create the "compileEcstasy" task for xdk project (draft)

--- xdk/build.gradle.kts
+++ xdk/build.gradle.kts
@@ -1,6 +1,9 @@
 /*
- * Build file for the XDK.
+ * Build files for the XDK.
  */
+
+val javatools    = project(":javatools")
+val javatoolsJar = "${javatools.buildDir}/libs/javatools.jar"
 
 val copyOutline = tasks.register<Copy>("copyOutline") {
     from(file("$projectDir/src/main/resources"))
@@ -9,6 +12,19 @@ val copyOutline = tasks.register<Copy>("copyOutline") {
 }
 
 val copyJavatools = tasks.register<Copy>("copyJavatools") {
-    from(file("${project(":javatools").buildDir}/lib/javatools.jar"))
+    from(file(javatoolsJar))
     into(file("$buildDir/xdk/javatools/"))
+
+    dependsOn(javatools.tasks["build"])
+}
+
+tasks.register<JavaExec>("compileEcstasy") {
+    group = "Execution"
+    description = "Build Ecstasy.xtc and _native.xtc modules"
+
+    dependsOn(javatools.tasks["build"])
+
+    println(javatoolsJar)
+    classpath(javatoolsJar)
+    main = "org.xvm.compiler.CommandLine"
 }
