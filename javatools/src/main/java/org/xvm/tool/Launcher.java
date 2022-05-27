dfaaa919829a943e324017563e5114629103019f
Gene Gleyzer <gene@xqiz.it>
2022-05-27 14:36:46 -0400

    Remove unused import

--- javatools/src/main/java/org/xvm/tool/Launcher.java
+++ javatools/src/main/java/org/xvm/tool/Launcher.java
@@ -11,7 +11,6 @@ import java.nio.file.Paths;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
