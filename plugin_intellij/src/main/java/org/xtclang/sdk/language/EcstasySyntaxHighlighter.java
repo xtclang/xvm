2634fc1d9fd18098e3cfbb3163cac38715d58462
Jackson Thea <jackson.thea1@gmail.com>
2022-11-13 13:51:19 -0500

    clean

--- plugin_intellij/src/main/java/org/xtclang/sdk/language/EcstasySyntaxHighlighter.java
+++ plugin_intellij/src/main/java/org/xtclang/sdk/language/EcstasySyntaxHighlighter.java
@@ -18,25 +18,6 @@ import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAtt
 
 public class EcstasySyntaxHighlighter extends SyntaxHighlighterBase
     {
-    public static final TextAttributesKey SEPARATOR =
-        createTextAttributesKey("ECSTASY_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
-    public static final TextAttributesKey KEY =
-        createTextAttributesKey("ECSTASY_KEY", DefaultLanguageHighlighterColors.KEYWORD);
-    public static final TextAttributesKey VALUE =
-        createTextAttributesKey("ECSTASY_VALUE", DefaultLanguageHighlighterColors.STRING);
-    public static final TextAttributesKey COMMENT =
-        createTextAttributesKey("ECSTASY_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-    public static final TextAttributesKey BAD_CHARACTER =
-        createTextAttributesKey("ECSTASY_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
-
-
-    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-    private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
-    private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
-    private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
-    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
-    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
-
     @NotNull
     @Override
     public Lexer getHighlightingLexer()
