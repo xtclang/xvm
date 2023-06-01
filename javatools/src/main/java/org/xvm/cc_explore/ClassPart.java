f7714117151acd97a2951d81c5b988915b35003d
Cliff Click <cliffc@acm.org>
2023-06-01 15:43:53 -0700

    Parse a Module OK
    Need to add a child component/part

--- javatools/src/main/java/org/xvm/cc_explore/ClassPart.java
+++ javatools/src/main/java/org/xvm/cc_explore/ClassPart.java
@@ -3,13 +3,35 @@ package org.xvm.cc_explore;
 import org.xvm.cc_explore.cons.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
    Class component
  */
 class ClassPart extends Part {
+  final HashMap<StringCon,TCon> _params; // String->Type mapping
+  final LitCon _path;           // File name compiling this file
   ClassPart( Part par, int nFlags, IdCon id, CondCon cond, FilePart X ) throws IOException {
     super(par,nFlags,id,cond,X);
-    // No other bits to parse
+    _params = parseTypeParams(X);
+    _path = (LitCon)X._pool.get(X.u31());
   }
+
+  // Helper method to read a collection of type parameters.
+  HashMap<StringCon, TCon> parseTypeParams(FilePart X) throws IOException {
+    int c = X.u31();
+    if( c <= 0 ) return null;
+
+    //ListMap<StringConstant, TypeConstant> map = new ListMap<>();
+    //ConstantPool pool = getConstantPool();
+    //for (int i = 0; i < c; ++i) {
+    //  StringConstant constName = (StringConstant) pool.getConstant(readIndex(in));
+    //  TypeConstant   constType = (TypeConstant)   pool.getConstant(readIndex(in));
+    //  assert !map.containsKey(constName);
+    //  map.put(constName, constType);
+    //}
+    //return map;
+    throw XEC.TODO();
+  }
+
 }
