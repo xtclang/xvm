f7bbf984c6821c3d6119e42834aace983ac6b14e
Cliff Click <cliffc@acm.org>
2023-06-07 13:08:01 -0700

    Parse constant pool of TestNumbers

---
+++ javatools/src/main/java/org/xvm/cc_explore/cons/Flt64Con.java
@@ -0,0 +1,14 @@
+package org.xvm.cc_explore.cons;
+
+import org.xvm.cc_explore.*;
+
+/**
+  Exploring XEC Constants
+ */
+public class Flt64Con extends Const {
+  public final double _flt;
+  public Flt64Con( FilePart X ) {
+    _flt = Double.longBitsToDouble(X.i64());
+  }
+  @Override public void resolve( CPool pool ) {}
+}
