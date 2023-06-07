a6f190214547d85dc2760af9e5d4e3f8fdcf7a33
Cliff Click <cliffc@acm.org>
2023-06-07 14:00:31 -0700

    Parse TestArray

---
+++ javatools/src/main/java/org/xvm/cc_explore/cons/CharCon.java
@@ -0,0 +1,13 @@
+package org.xvm.cc_explore.cons;
+
+import org.xvm.cc_explore.*;
+import java.math.BigInteger;
+
+/**
+  Exploring XEC Constants
+ */
+public class CharCon extends Const {
+  private final int _ch;
+  public CharCon( FilePart X ) { _ch = X.utf8Char(); }
+  @Override public void resolve( CPool pool ) {}
+}
