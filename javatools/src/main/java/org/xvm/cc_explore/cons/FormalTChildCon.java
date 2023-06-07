04af4e4ae5a532b8290f40dcbce0a9ed3a69cace
Cliff Click <cliffc@acm.org>
2023-06-07 14:00:31 -0700

    Parse TestArray

---
+++ javatools/src/main/java/org/xvm/cc_explore/cons/FormalTChildCon.java
@@ -0,0 +1,10 @@
+package org.xvm.cc_explore.cons;
+
+import org.xvm.cc_explore.*;
+
+/**
+  Exploring XEC Constants
+ */
+public class FormalTChildCon extends PropCon {
+  public FormalTChildCon( FilePart X ) { super(X); }
+}
