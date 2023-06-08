5c50ea3bb57ad1e395cf06e661fe55d7dca94e2d
Cliff Click <cliffc@acm.org>
2023-06-08 11:23:14 -0700

    All the XTC in XDK parses constant pool

---
+++ javatools/src/main/java/org/xvm/cc_explore/cons/FPNCon.java
@@ -0,0 +1,21 @@
+package org.xvm.cc_explore.cons;
+
+import org.xvm.cc_explore.CPool;
+import org.xvm.cc_explore.FilePart;
+
+/**
+  Exploring XEC Constants
+ */
+public class FPNCon extends Const {
+  final Format _f;
+  private final byte[] _buf;
+  public FPNCon( FilePart X, Format f ) {
+    _f = f;
+    int len = (1 << X.u8());
+    _buf = X.bytes(len);
+    int min = f==Format.DecN ? 4 : 2;
+    if( len < min || len > 16384 )
+      throw new IllegalArgumentException("value length ("+len+") must be a power-of-two between " + min + " and 16384");
+  }
+  @Override public void resolve( CPool pool ) { }
+}
