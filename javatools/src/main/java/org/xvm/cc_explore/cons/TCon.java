c84d62537c7fb432308a63df5d6e7f2b0364ca3e
Cliff Click <cliffc@acm.org>
2023-06-13 13:41:53 -0700

    Actually load all provided libs
    Was not loading (lazy) 2-deep nested kids; fixed now loading all things.
    Loading Methods with code.
    
    Short cuts for loading arrays of Const, TCon, Annots.
    Added some toStrings.

--- javatools/src/main/java/org/xvm/cc_explore/cons/TCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/TCon.java
@@ -1,9 +1,19 @@
 package org.xvm.cc_explore.cons;
 
+import org.xvm.cc_explore.CPool;
 import org.xvm.cc_explore.XEC;
 
 /**
   Exploring XEC Constants
  */
 public abstract class TCon extends Const {
+  // Resolve an array
+  public static TCon[] resolveAry( CPool pool, int[] xs) {
+    TCon[] cs = new TCon[xs.length];
+    for( int i=0; i<xs.length; i++ )
+      cs[i] = (TCon)pool.get(xs[i]);
+    return cs;
+  }
+
+
 }
