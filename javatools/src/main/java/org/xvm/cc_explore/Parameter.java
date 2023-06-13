f05137aac078b1589d96ae8200b9df4c719c468d
Cliff Click <cliffc@acm.org>
2023-06-13 13:41:53 -0700

    Actually load all provided libs
    Was not loading (lazy) 2-deep nested kids; fixed now loading all things.
    Loading Methods with code.
    
    Short cuts for loading arrays of Const, TCon, Annots.
    Added some toStrings.

---
+++ javatools/src/main/java/org/xvm/cc_explore/Parameter.java
@@ -0,0 +1,31 @@
+package org.xvm.cc_explore;
+
+import org.xvm.cc_explore.cons.*;
+
+public class Parameter {
+  // Parameter annotations
+  public final Annot[] _annots;
+  // Parameter type
+  public final TCon _type;
+  // Parameter name
+  public final StringCon _name;
+  // Default value
+  public final Const _def;
+  // True IFF a condition-return or a type-parm parm
+  public final boolean _special;
+  // Parameter index, negative for returns
+  public final int _idx;
+  
+  Parameter( boolean is_ret, int idx, boolean special, FilePart X ) {    
+    _annots = Annot.xannos(X);
+    _type = (TCon)X.xget();
+    _name = (StringCon)X.xget();
+    _def  = X.xget();
+    _idx = is_ret ? -1-idx : idx;
+    _special = special;
+  }
+
+  @Override public String toString() {
+    return _name==null ? "#"+_idx : _name.toString();
+  }
+}
---
+++ javatools/src/main/java/org/xvm/cc_explore/XRuntime.java
@@ -0,0 +1,62 @@
+package org.xvm.cc_explore;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+   Support for thread pools, perhaps other system and runtime services.  One
+   per JVM, per physical hardware.  Shared amongst all Containers and services.
+*/
+public abstract class XRuntime {
+  //  The executor for XVM services.
+  public static final ThreadPoolExecutor _executorXVM;
+
+  // The executor for XVM services.
+  public static final ThreadPoolExecutor _executorIO;
+  
+  static {
+    int parallelism = Integer.parseInt(System.getProperty("xvm.parallelism", "0"));
+    if (parallelism <= 0)
+      parallelism = java.lang.Runtime.getRuntime().availableProcessors();
+
+    ThreadGroup groupXVM = new ThreadGroup("XVM");
+    ThreadFactory factoryXVM = r -> {
+      Thread thread = new Thread(groupXVM, r);
+      thread.setDaemon(true);
+      thread.setName("XvmWorker@" + thread.hashCode());
+      return thread;
+    };
+
+    // TODO: replace with a fair scheduling based ExecutorService; and a concurrent blocking queue
+    _executorXVM = new ThreadPoolExecutor(parallelism, parallelism, 0, TimeUnit.SECONDS,  new LinkedBlockingQueue<>(), factoryXVM);
+
+    ThreadGroup groupIO = new ThreadGroup("IO");
+    ThreadFactory factoryIO = r -> {
+      Thread thread = new Thread(groupIO, r);
+      thread.setDaemon(true);
+      thread.setName("IOWorker@" + thread.hashCode());
+      return thread;
+    };
+
+    _executorIO = new ThreadPoolExecutor(parallelism, 1024, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), factoryIO);
+  }
+
+  
+  /** Submit ServiceContext work for eventual processing by the runtime.
+   *  @param task the task to process   */
+  static protected void submitService(Runnable task) { _executorXVM.submit(task); }
+
+  /** Submit IO work for eventual processing by the runtime.
+   *  @param task the task to process    */
+  protected static void submitIO(Runnable task) { _executorIO.submit(task); }
+
+  public static void start() {}
+
+  public static void shutdownXVM() {
+    _executorIO .shutdown();
+    _executorXVM.shutdown();
+  }
+}
