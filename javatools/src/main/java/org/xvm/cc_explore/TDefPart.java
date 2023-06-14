a5fb540a6c376bba2ecf794a5d238f3bbf16a88d
Cliff Click <cliffc@acm.org>
2023-06-13 19:38:18 -0700

    Link the repo
    Also drop failed generify of Part (more errors and warnigns than casts saved)

--- javatools/src/main/java/org/xvm/cc_explore/TDefPart.java
+++ javatools/src/main/java/org/xvm/cc_explore/TDefPart.java
@@ -5,7 +5,7 @@ import org.xvm.cc_explore.cons.*;
 /**
    Type Definition part
  */
-class TDefPart extends Part<TDefCon> {
+class TDefPart extends Part {
   public final TCon _type;
   TDefPart( Part par, int nFlags, TDefCon id, CondCon cond, FilePart X ) {
     super(par,nFlags,id,cond,X);
