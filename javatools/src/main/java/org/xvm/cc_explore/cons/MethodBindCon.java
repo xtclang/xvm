625485cf0cae03f2b4016ab1acdf4714d1c21cf5
Cliff Click <cliffc@acm.org>
2023-06-17 09:35:44 -0700

    Progress on linking
    Linking should remove the need for the CPool everywhere.  Recursively find all XXXCon items and add a cached XXXPart instead.  This search crosses CPool borders in some cases and thus requires the full Repo.  Handles interface method lookups, but not globals like toString.
    All the XXXCon classes cache something after linking, so a 2nd call to link does nothing (as a form of visit bit).  The XXXPart classes use an external visit bit to avoid double-linking.
    Hide more of the XXXCon fields and use the XXXPart fields instead.
    Replace more StringCon with interned String.
    MethodPart is now a MMEthodPart with more stuff; MMethods retain the notion of a collection of Methods, and both can be stored in a MMethod array.

--- javatools/src/main/java/org/xvm/cc_explore/cons/MethodBindCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/MethodBindCon.java
@@ -7,6 +7,10 @@ import org.xvm.cc_explore.*;
  */
 public class MethodBindCon extends Const {
   private MethodCon _method;
+  private MethodPart _meth;
   public MethodBindCon( CPool X ) { X.u31(); }
   @Override public void resolve( CPool X ) { _method = (MethodCon)X.xget(); }
+  @Override public Part link(XEC.ModRepo repo) {
+    return _meth==null ? (_meth=_method.link(repo)) : _meth;
+  }
 }
