fd89276e3e1f7e640cb0eb6015c05dc3d2ab2611
Cliff Click <cliffc@acm.org>
2023-06-20 11:39:44 -0700

    Child lookup must link.
    Class lookup in both Implements and Extends.
    Cleanup MMethod linking.
    Handle AnonClz, Formal, ParClz.

--- javatools/src/main/java/org/xvm/cc_explore/cons/MMethodCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/MMethodCon.java
@@ -7,4 +7,12 @@ import org.xvm.cc_explore.*;
  */
 public class MMethodCon extends NamedCon {
   public MMethodCon( CPool X ) { super(X); }
+  @Override public Part link( XEC.ModRepo repo ) {
+    Part p = super.link(repo);
+    if( p!=null ) return p;
+    // If parent lookup fails, try again in Object
+    p = repo.get("ecstasy.xtclang.org").child("Object",repo).child(name(),repo);
+    assert p!=null;
+    return (_part=p);    
+  }
 }
