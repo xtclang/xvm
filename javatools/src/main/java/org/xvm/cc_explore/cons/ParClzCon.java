643f5b5309f40416abb1d97e4e807236c064ee7f
Cliff Click <cliffc@acm.org>
2023-06-20 11:39:44 -0700

    Child lookup must link.
    Class lookup in both Implements and Extends.
    Cleanup MMethod linking.
    Handle AnonClz, Formal, ParClz.

--- javatools/src/main/java/org/xvm/cc_explore/cons/ParClzCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/ParClzCon.java
@@ -10,5 +10,9 @@ public class ParClzCon extends PartCon {
   public ParClzCon( CPool X ) { X.u31();  }
   @Override public void resolve( CPool X ) { _child = (IdCon)X.xget(); }
   @Override public String name() { return _child.name(); }
-  @Override public IdCon link_as(XEC.ModRepo repo) { throw XEC.TODO(); }
+  @Override public Part link( XEC.ModRepo repo ) {
+    if( _part!=null ) return _part;
+    assert _par==null;
+    return (_part = _child.link(repo));
+  }
 }
