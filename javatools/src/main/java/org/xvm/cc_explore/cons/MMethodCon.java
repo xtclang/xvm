1a534c35f0388446c743029c654d9d4b010fa262
Cliff Click <cliffc@acm.org>
2023-06-20 10:05:54 -0700

    Yank generic type
    Added 20 lines to avoid 2 casts

--- javatools/src/main/java/org/xvm/cc_explore/cons/MMethodCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/MMethodCon.java
@@ -5,6 +5,6 @@ import org.xvm.cc_explore.*;
 /**
   Exploring XEC Constants
  */
-public class MMethodCon extends NamedCon<MMethodPart> {
+public class MMethodCon extends NamedCon {
   public MMethodCon( CPool X ) { super(X); }
 }
