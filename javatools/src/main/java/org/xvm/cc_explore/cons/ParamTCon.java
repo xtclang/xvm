6f8f9a5c4a2fe6127a14640ffa95a6a7149399df
Cliff Click <cliffc@acm.org>
2023-06-23 13:04:27 -0700

    Handling multiple methods under a MultiMethod
    More improved prints.
    More null-vs-zero-length arrays

--- javatools/src/main/java/org/xvm/cc_explore/cons/ParamTCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/ParamTCon.java
@@ -11,11 +11,12 @@ public class ParamTCon extends TCon {
   TCon _con;
   TCon[] _parms;
   ClassPart _clz;
-  XType[] _types;
+  public final XType[] _types;
   
   public ParamTCon( CPool X ) {
     X.u31();
-    X.skipAry();
+    int len = X.skipAry();
+    _types = len==0 ? null : new XType[len];
   }
   @Override public SB str(SB sb) {
     if( _con instanceof TermTCon tt ) sb.p(tt.name());
@@ -30,7 +31,6 @@ public class ParamTCon extends TCon {
     if( _clz!=null ) return _clz;
     _clz = (ClassPart)_con.link(repo);
     if( _parms!=null ) {
-      _types = new XType[_parms.length];
       for( int i=0; i<_parms.length; i++ )
         _types[i] = _parms[i].link(repo);
     }
