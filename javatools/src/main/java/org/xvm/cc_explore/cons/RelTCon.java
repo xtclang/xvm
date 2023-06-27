8fcd2c41795b5c96ca516a4586b55928756ea458
Cliff Click <cliffc@acm.org>
2023-06-27 10:08:05 -0700

    Drop XType, add NBHML, TVar.
    Move Contribs to its own file.  Move _contribs to Class and Prop and out of Part.
    Add NBHML and helper methods to util.
    Add TVar,TVLeaf.
    Hooks for computing TVar.
    Add CPool.isDigit to remove hook on Handy, to allow make/clean/make without gradle.

--- javatools/src/main/java/org/xvm/cc_explore/cons/RelTCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/RelTCon.java
@@ -7,7 +7,7 @@ import org.xvm.cc_explore.*;
  */
 public abstract class RelTCon extends TCon {
   TCon  _con1,  _con2;
-  private XType _type1, _type2;
+  private Part _type1, _type2;
   public RelTCon( CPool X ) {
     X.u31();
     X.u31();
@@ -16,7 +16,7 @@ public abstract class RelTCon extends TCon {
     _con1 = (TCon)X.xget();
     _con2 = (TCon)X.xget();
   }
-  @Override public XType link(XEC.ModRepo repo) {
+  @Override public Part link(XEC.ModRepo repo) {
     if( _type1!=null ) return _type1;
     _type2 = _con2.link(repo);
     return (_type1 = _con1.link(repo));
