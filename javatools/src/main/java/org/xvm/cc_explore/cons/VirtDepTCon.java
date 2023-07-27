4440615fbd325ba430b4662be47ba651a9cd626d
Cliff Click <cliffc@acm.org>
2023-07-27 10:05:43 -0700

    Fix ints after rebase from master
    Update minor version.
    Start on opcode parsing.
    Start on first native method.

--- javatools/src/main/java/org/xvm/cc_explore/cons/VirtDepTCon.java
+++ javatools/src/main/java/org/xvm/cc_explore/cons/VirtDepTCon.java
@@ -2,7 +2,6 @@ package org.xvm.cc_explore.cons;
 
 import org.xvm.cc_explore.*;
 import org.xvm.cc_explore.util.SB;
-import static org.xvm.cc_explore.Part.Composition.*;
 
 /**
   Exploring XEC Constants
@@ -22,26 +21,11 @@ public class VirtDepTCon extends DepTCon {
   }
   @Override public Part link(XEC.ModRepo repo) {
     if( _part!=null ) return _part;
-    ClassPart par = (ClassPart)_par.link(repo);
-    _part = search(par,null);
+    Part par = _par.link(repo);
+    while( !(par instanceof ClassPart clz) )
+      par = par._par;
+    _part = clz.child(_name);
     assert _part!=null;
     return _part;
   }
-
-  // Hunt this clz for name, plus recursively any Implements contribs.
-  // Assert only 1 found
-  Part search( ClassPart clz, Part p ) {
-    Part p0 = clz.child(_name);
-    if( p0!=null ) {
-      assert p==null || p==p0;
-      return p0;
-    }
-    if( clz._contribs != null )
-      for( Contrib c : clz._contribs )
-        if( c._comp==Implements || c._comp==Into || c._comp==Extends )
-          p = search(((ClzCon)c._tContrib).clz(),p);
-    return p;
-  }
-  
-  
 }
