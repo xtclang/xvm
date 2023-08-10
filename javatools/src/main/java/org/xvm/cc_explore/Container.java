2a7f8941a8a932481391bcfaca4327bd8b25c70f
Cliff Click <cliffc@acm.org>
2023-08-10 11:09:32 -0700

    All the ops for Permutation's main method
    Fails in pass#2 for bytecode layout, but gets all the ops in pass#1.
    CALL_11, NVOK_11, FBIND, NEWG_1, NEWG_N, RETURN_1, JMP_NEQ, JMP_LT, JMP_GT, JMP_GTE, GP_SUB, GP_IRANGEE, P_GET, IP_DEC, I_GET

--- javatools/src/main/java/org/xvm/cc_explore/Container.java
+++ javatools/src/main/java/org/xvm/cc_explore/Container.java
@@ -20,5 +20,22 @@ public abstract class Container {
     XClz xclz = _mod.xclz();
     throw XEC.TODO();
   }
+
+  // TODO:
+  // Top-level design thinking:
+  
+  // Move an instanceof XClzCompiler here.  Need a private Java ClassLoader per
+  // Container; might as well have the XCompiler support here.  The private
+  // classLoaders means XTC Container classes are unrelated to each other and
+  // can e.g. name-shadown.  All modules in a container should have a "blank"
+  // or "simple" top-level name, since they already have a private classLoader
+  // and so already have namespace.
+  
+  // Example: module "FizzBuzz" gets full Java class name "XTC.JFizzBuzz", and
+  // contains the module constructor in the Java statics.
+  
+  // Classes within a module get the module as a file-path element in their name.
+  // Example: class "FizzBuzz" has file Java name "XTC.JFizzBuzz.JFizzBuzz".
+
   
 }
