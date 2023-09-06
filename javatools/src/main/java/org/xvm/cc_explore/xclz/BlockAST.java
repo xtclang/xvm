276d661b802a91737350d6e2eeebd402a3e3b575
Cliff Click <cliffc@acm.org>
2023-09-06 14:39:15 -0700

    Handle more AST
    Back to using the MethodPart pool.
    No need to set/restore the CPool parser.
    Add a bunch of "_ast" parser bits, will probably toss the old stuff next go 'round.

--- javatools/src/main/java/org/xvm/cc_explore/xclz/BlockAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/BlockAST.java
@@ -1,10 +1,9 @@
 package org.xvm.cc_explore.xclz;
 
-import org.xvm.cc_explore.CPool;
 import org.xvm.cc_explore.util.SB;
 
 class BlockAST extends AST {
-  BlockAST( CPool X ) { super(X, X.u31()); }
+  BlockAST( XClzBuilder X ) { super(X, X.u31()); }
   @Override void jpre ( SB sb ) {  sb.ip("{").ii().nl();  }
   @Override void jpost( SB sb ) {  sb.di().ip("}").nl();  }
 }
