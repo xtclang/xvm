542ef721844c73e42e85bbfdbce895d71bb86624
Cliff Click <cliffc@acm.org>
2023-09-13 16:32:56 -0700

    Run HelloWorld
    parse ast_op - peeks 1st byte, if negative, that's the value.  if positive use the full packed64.
    Parse invokes, constants, default "register".  Fill in default values.
    Push/pop local names on scope change.
    XConsole print takes a boolean for newln

--- javatools/src/main/java/org/xvm/cc_explore/XConsole.java
+++ javatools/src/main/java/org/xvm/cc_explore/XConsole.java
@@ -4,7 +4,9 @@ package org.xvm.cc_explore;
 */
 public class XConsole {
 
-  public void print( String s ) {
-    System.out.println(s);
+  public void print( String s, boolean no_newline ) {
+    System.out.print(s);
+    if( !no_newline )
+      System.out.println();
   }
 }
