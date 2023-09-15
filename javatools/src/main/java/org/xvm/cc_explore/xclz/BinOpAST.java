7c65770b0691b8da7819007618fff4f8e74808ac
Cliff Click <cliffc@acm.org>
2023-09-14 19:52:29 -0700

    Progress on AST towards Permute.x

--- javatools/src/main/java/org/xvm/cc_explore/xclz/BinOpAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/BinOpAST.java
@@ -5,13 +5,15 @@ import org.xvm.cc_explore.XEC;
 import org.xvm.cc_explore.util.SB;
 
 class BinOpAST extends AST {
-  final Operator _op;
   static final Operator[] OPS = Operator.values();
-  BinOpAST( XClzBuilder X ) {
+  final Operator _op;
+  final String _type;
+  BinOpAST( XClzBuilder X, boolean type ) {
     super(X, 2, false);
     _kids[0] = ast_term(X);
     _op = OPS[X.u31()];
     _kids[1] = ast_term(X);
+    _type = type ? X.jtype_methcon_ast() : null;
   }
   @Override void jpre ( SB sb ) { }
   @Override void jmid ( SB sb, int i ) { if( i==0 ) sb.p(_op.text); }
