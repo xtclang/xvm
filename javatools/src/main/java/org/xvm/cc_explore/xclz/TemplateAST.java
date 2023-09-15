01b64f195460acb50c41fd8ef7c772451a463e69
Cliff Click <cliffc@acm.org>
2023-09-15 14:17:14 -0700

    Progress on Permutation.x
    Mostly minor printing stuff; TemplateAST.

--- javatools/src/main/java/org/xvm/cc_explore/xclz/TemplateAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/TemplateAST.java
@@ -5,9 +5,12 @@ import org.xvm.cc_explore.util.SB;
 
 class TemplateAST extends AST {
   TemplateAST( XClzBuilder X ) {
-    super(X, X.u31(), false);
+    super(X, X.u31()-1, false);
+    AST ast = ast_term(X);      // Toss away the StringBuilder first op
     for( int i=0; i<_kids.length; i++ )
       _kids[i] = ast_term(X);
   }
-  @Override void jpre ( SB sb ) { throw XEC.TODO(); }
+  @Override void jpre ( SB sb ) { sb.p("\"\"+");  }
+  @Override void jmid ( SB sb, int i ) { sb.p("+"); }
+  @Override void jpost( SB sb ) { sb.unchar(1); }  
 }
