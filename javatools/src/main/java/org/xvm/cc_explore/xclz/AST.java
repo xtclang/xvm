64be7ff8c94f4aae2a526b51627c1e158102a4ff
Cliff Click <cliffc@acm.org>
2023-09-17 16:13:03 -0700

    Progress Permutations.x
    Handles FizzBuzz.x also.
    First cut Switch, ForRange, and Range.
    More smarts about NL & indenting.
    Add a trailing NL test to SB.

--- javatools/src/main/java/org/xvm/cc_explore/xclz/AST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/AST.java
@@ -7,6 +7,7 @@ import org.xvm.cc_explore.util.SB;
 public abstract class AST {
   final AST[] _kids;
   final int _nlocals;           // Count of locals before parsing nested kids
+  AST _par;
 
   // Parse kids in order, save/restore locals around kids
   AST( XClzBuilder X, int n ) { this(X,n,true); }
@@ -22,25 +23,68 @@ public abstract class AST {
     // Pop locals at end of scope
     X.pop_locals(_nlocals);
   }
+  // Simple all-fields constructor
+  AST( AST[] kids, int nlocals ) {
+    _kids = kids;
+    _nlocals = nlocals;
+  }
 
-  @Override public String toString() { return jcode(new SB()).toString(); }
+  @Override public String toString() { return jcode(new SB() ).toString(); }
 
+  // Use the _par parent to find nearest enclosing Block for tmps
+  BlockAST enclosing() {
+    AST ast = this;
+    while( !(ast instanceof BlockAST blk) ) ast = ast._par;
+    return blk;
+  }
   
-  final SB jcode(SB sb) {
+  // String java Type of this expression
+  String type() { throw XEC.TODO(); }
+  
+  // Walk, and allow AST to rewrite themselves in-place.
+  // Set the _par parent.
+  final AST do_rewrite() {
+    if( _kids!=null )
+      for( int i=0; i<_kids.length; i++ ) {
+        AST kid = _kids[i], old;
+        do {          
+          kid._par = this;
+          kid = (old=kid).rewrite();
+        } while( old!=kid );
+        _kids[i] = kid;
+        _kids[i].do_rewrite();
+      }
+    return this;
+  }
+  
+  // Rewrite some AST bits before Java
+  AST rewrite() { return this; }
+  
+  /**
+   * Dump indented pretty java code from AST.
+   *
+   * @param sb - dump into this SB
+   */
+  SB jcode( SB sb ) {
+    if( sb.was_nl() ) sb.i();
     jpre(sb);
     if( _kids!=null )
       for( int i=0; i<_kids.length; i++ ) {
         if( _kids[i]==null ) continue;
-        _kids[i].jcode(sb);
+        _kids[i].jcode(sb );
         jmid(sb, i);
       }
     jpost(sb);
     return sb;
   }
-  abstract void jpre ( SB sb );
-  void jpost( SB sb ) {}
+  
+  // Pretty-print in Java overrides.  Returns a flag to do a visit over all
+  // children (with jmid in between kids), or just allow jpre to do the entire
+  // print.
+  void jpre ( SB sb ) {}
   void jmid ( SB sb, int i ) {}
-
+  void jpost( SB sb ) {}
+  
   public static AST parse( XClzBuilder X ) { return ast(X); }
 
   static AST ast_term( XClzBuilder X ) {
@@ -63,9 +107,11 @@ public abstract class AST {
     case Assign       -> new   AssignAST(X,true);
     case CallExpr     -> new     CallAST(X, X.consts());
     case CondOpExpr   -> new    BinOpAST(X,false);
+    case ForRangeStmt -> new ForRangeAST(X);
     case IfElseStmt   -> new       IfAST(X,3);
     case IfThenStmt   -> new       IfAST(X,2);
     case InvokeExpr   -> new   InvokeAST(X, X.consts());
+    case MultiExpr    -> new    MultiAST(X);
     case NamedRegAlloc-> new   DefRegAST(X,true ,false);
     case NewExpr      -> new      NewAST(X, X.methcon_ast(), X.methcon_ast());
     case NotImplYet   -> new     TODOAST(X);
@@ -74,6 +120,7 @@ public abstract class AST {
     case Return0Stmt  -> new   ReturnAST(X,0);
     case Return1Stmt  -> new   ReturnAST(X,1);
     case StmtBlock    -> new    BlockAST(X);
+    case SwitchExpr   -> new   SwitchAST(X, ast_term(X), X.pack64(), X.consts());
     case TemplateExpr -> new TemplateAST(X);
     case TernaryExpr  -> new  TernaryAST(X);
     
