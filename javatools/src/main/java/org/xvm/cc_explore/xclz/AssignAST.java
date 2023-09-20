a57a62129ae57b7698eee885f2097b0d3f9ed3c5
Cliff Click <cliffc@acm.org>
2023-09-20 11:29:09 -0700

    1/2 flip to a new (old) binary parser

--- javatools/src/main/java/org/xvm/cc_explore/xclz/AssignAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/AssignAST.java
@@ -6,11 +6,13 @@ import org.xvm.cc_explore.util.SB;
 class AssignAST extends AST {
   static final Operator[] OPS = Operator.values();
   final Operator _op;
-  AssignAST( XClzBuilder X, boolean asgn ) {
-    super(X, 2, false);
-    _kids[0] = ast_term(X);
-    _op = asgn ? Operator.Asn : OPS[X.u31()];
-    _kids[1] = ast_term(X);
+  static AssignAST make( XClzBuilder X, boolean asgn ) {
+    AST[] kids = new AST[2];
+    kids[0] = ast_term(X);
+    Operator op = asgn ? Operator.Asn : OPS[X.u31()];
+    kids[1] = ast_term(X);
+    return new AssignAST(kids, op);
   }
-  @Override void jmid ( SB sb, int i ) { if( i==0 ) sb.p(' ').p(_op.text).p(' '); }
+  private AssignAST( AST[] kids, Operator op ) { super(kids); _op=op; }
+  @Override void jmid( SB sb, int i ) { if( i==0 ) sb.p(' ').p(_op.text).p(' '); }
 }
