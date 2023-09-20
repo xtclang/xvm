ca3c3dfb29c8806bc530153d1a2f06b6b6a60640
Cliff Click <cliffc@acm.org>
2023-09-20 11:29:09 -0700

    1/2 flip to a new (old) binary parser

--- javatools/src/main/java/org/xvm/cc_explore/xclz/MultiAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/MultiAST.java
@@ -3,7 +3,14 @@ package org.xvm.cc_explore.xclz;
 import org.xvm.cc_explore.util.SB;
 
 class MultiAST extends AST {
-  MultiAST( XClzBuilder X ) { super(X, X.u31()); }
+  static MultiAST make(XClzBuilder X) {
+    int len = X.u31();
+    AST[] kids = new AST[len];
+    for( int i=0; i<len; i++ )
+      kids[i] = ast_term(X);
+    return new MultiAST(kids);
+  }
+  MultiAST( AST[] kids ) { super(kids); }
   @Override void jpre ( SB sb ) { sb.p("("); }
   @Override void jmid ( SB sb, int i ) { sb.p(", "); }
   @Override void jpost( SB sb ) { sb.unchar(2).p(")"); }
