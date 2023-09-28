796980e722c88c87f81c021e33bece217888b998
Cliff Click <cliffc@acm.org>
2023-09-28 13:15:32 -0700

    More tuple support
    addAll, equals, at(range), slice
    More tuple constructors.
    Calling 1.toInt() turns into Long.valueOf(1)

--- javatools/src/main/java/org/xvm/cc_explore/xrun/Tuple0.java
+++ javatools/src/main/java/org/xvm/cc_explore/xrun/Tuple0.java
@@ -1,7 +1,7 @@
 package org.xvm.cc_explore.xrun;
 import org.xvm.cc_explore.XEC;
 public class Tuple0 extends Tuple {
-  public final int len() { return 0; }
+  public Tuple0() { super(0); }
   public final Object at(int i) { throw XEC.TODO(); }
   public final void set(int i, Object e) { throw XEC.TODO(); }
 }
