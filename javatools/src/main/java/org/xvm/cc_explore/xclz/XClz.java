f92b6b2ace7a22c68b495471df92071cbcae9e79
Cliff Click <cliffc@acm.org>
2023-09-28 10:23:32 -0700

    Handle dynamic tuples

--- javatools/src/main/java/org/xvm/cc_explore/xclz/XClz.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/XClz.java
@@ -4,16 +4,24 @@ import org.xvm.cc_explore.xrun.*;
 
 // Some kind of base class for a Java class that implements an XTC Module
 public abstract class XClz {
-  private final byte _mutability;
-  public XClz() { _mutability=0; }
-  public XClz(byte mut) { _mutability=mut; }
   // Default mutability
-  public Mutability mutability$get() { return Mutability.VALUES[_mutability]; }
+  public Mutability mutability$get() { return Mutability.Constant; }
 
+  // Trace
+  public final <T> T TRACE() {
+    return (T)this;
+  }
+
+  // Assert is always-on runtime test
+  public static void xassert( boolean cond ) {
+    if( !cond ) throw new AssertionError();
+  }
+  
   public enum Mutability {
     Constant,                   // Deeply immutable
     Persistant,                 // Odd name, but shallow immutable
     Fixed;                      // Tuples and arrays are fixed length, but mutable
     static final Mutability[] VALUES = values();
-  }    
+  }
+
 }
