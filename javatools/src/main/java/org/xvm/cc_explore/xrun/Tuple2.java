4924c9eaf02d21571f2fcf2023c0f95bd6c836b8
Cliff Click <cliffc@acm.org>
2023-10-02 10:10:40 -0700

    Yank tvars
    Thought they might be needed, not needed yet.  Will revisit if problems show up in mixin composition.
    Another little bit of tuple improvements.

--- javatools/src/main/java/org/xvm/cc_explore/xrun/Tuple2.java
+++ javatools/src/main/java/org/xvm/cc_explore/xrun/Tuple2.java
@@ -1,24 +1,11 @@
 package org.xvm.cc_explore.xrun;
 import org.xvm.cc_explore.XEC;
-public abstract class Tuple2 extends Tuple {
-  public Tuple2() { super(2); }
-  public Object at(int i) {
-    return switch( i ) {
-    case 0 -> f0();
-    case 1 -> f1();
-    default -> throw XEC.TODO();
-    };
-  }
-  public final void set(int i, Object e) {
-    switch( i ) {
-    case 0 -> f0(e);
-    case 1 -> f1(e);
-    default -> throw XEC.TODO();
-    };
-  }
-  public abstract Object f0();
+public abstract class Tuple2 extends Tuple1 {
+  public Tuple2() { this(2); }
+  public Tuple2(int n) { super(n); }
+  public Object at(int i) { return i==1 ? f1() : super.at(i); }
+  public void set(int i, Object e) { if( i==1 ) f1(e); else super.set(i,e); }
   public abstract Object f1();
-  public abstract void f0(Object e);
   public abstract void f1(Object e);
 }
 
