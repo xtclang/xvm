9707e4ffe3c54bec6003ca0c885133c0edce1e29
Cliff Click <cliffc@acm.org>
2023-10-02 12:54:49 -0700

    Progress on misc tests
    Rename misc.x to TestMisc.x to match the module name.
    Comment out tests not currently working.
    Add print to Range; handle Inteval and Iterator as just another Range.
    UniOp partial support for converts; support for Boolean "~"
    BinOp all operator precedences.
    Template strings add some spaces.

--- javatools/src/main/java/org/xvm/cc_explore/xrun/Range.java
+++ javatools/src/main/java/org/xvm/cc_explore/xrun/Range.java
@@ -8,8 +8,17 @@ import java.util.Iterator;
 */
 abstract class Range implements Iterable<Long> {
   final long _lo, _hi;          // Inclusive lo, exclusive hi
-  Range( long lo, long hi ) { _lo=lo; _hi=hi; }
+  final boolean _lx, _hx;       // Declared inclusive/exclusive
+  Range( long lo, long hi, boolean lx, boolean hx ) { _lo=lo; _hi=hi; _lx=lx; _hx=hx; }
 
+  @Override
+  public final String toString() {
+    return
+      (_lx ? "["+_lo : "("+(_lo-1)) +
+      ".." +
+      (!_hx ? ""+_hi+")" : ""+(_hi-1)+"]");
+  }
+  
   /** @return an iterator */
   @Override public Iterator<Long> iterator() { return new Iter(); }
   private class Iter implements Iterator<Long> {
