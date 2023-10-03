1e0e66e69d9d40fd0ddeb24b4d40da283b43de6b
Cliff Click <cliffc@acm.org>
2023-10-03 10:14:24 -0700

    A few more switch variants
    Compiling with java 17 and enable-preview on the gen'd code
    Breaks & continues 1 level deep.
    While loops.
    pre/post dec/inc.
    multi expr of 1
    Smarter BinOp paren grouping

--- javatools/src/main/java/org/xvm/cc_explore/xclz/MultiAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/MultiAST.java
@@ -1,17 +1,20 @@
 package org.xvm.cc_explore.xclz;
 
 import org.xvm.cc_explore.util.SB;
+import org.xvm.cc_explore.XEC;
 
 class MultiAST extends AST {
-  static MultiAST make(XClzBuilder X) {
+  static MultiAST make(XClzBuilder X, boolean expr) {
     int len = X.u31();
     AST[] kids = new AST[len];
     for( int i=0; i<len; i++ )
-      kids[i] = ast_term(X);
+      kids[i] = expr ? ast_term(X) : ast(X);
     return new MultiAST(kids);
   }
   MultiAST( AST[] kids ) { super(kids); }
-  @Override void jpre ( SB sb ) { sb.p("("); }
-  @Override void jmid ( SB sb, int i ) { sb.p(", "); }
-  @Override void jpost( SB sb ) { sb.unchar(2).p(")"); }
+  @Override void jpre(SB sb) {
+    sb.nl().ii().i();
+    if( _kids.length > 1 ) throw XEC.TODO(); // Blocks need "{}", but how exprs?
+  }
+  @Override void jpost(SB sb) { sb.di(); }
 }
