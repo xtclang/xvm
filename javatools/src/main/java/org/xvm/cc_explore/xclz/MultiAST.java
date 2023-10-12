0dbd895ff96c2971a3b45518cad109aa1072a65e
Cliff Click <cliffc@acm.org>
2023-10-12 12:42:29 -0700

    Conditions!
    Conditions are not 1st class values, so can be always be passed as 2 parts with the boolean in a global, and the other part becomes the normal return type.
    TODO: multi-threaded.
    MultiAst with >1kid is "(e0 && e1 && ... )"
    Condition Returns explicitly call SET$COND()
    Methods flagged as conditional returns.  See large comment in MethodPart.
    Handle elvis ops:  (e0?.e1 ... : alt ) ==> (e0==null ? alt : e0.e1)
    Handle elvis ops:  (e0?.e1 , alts ) ==> ((e0==null || e0.e1) && alts)
    Minor restructure BinOp, Block to allow Java "...".
    UniOp.is_elvis
    IfAST print handles else clauses
    Bugfix: DefReg does not define BAST name "$"
    NarrowAST is a cast, typically a no-op since already type-checked.
    Prop: "string.size$get()" ==> "string.length()"
    assert UniOp "e0.TRACE()" ==> "XClz.TRACE(e0)"

--- javatools/src/main/java/org/xvm/cc_explore/xclz/MultiAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/MultiAST.java
@@ -2,6 +2,7 @@ package org.xvm.cc_explore.xclz;
 
 import org.xvm.cc_explore.util.SB;
 import org.xvm.cc_explore.XEC;
+import java.util.Arrays;
 
 class MultiAST extends AST {
   static MultiAST make(XClzBuilder X, boolean expr) {
@@ -11,10 +12,50 @@ class MultiAST extends AST {
       kids[i] = expr ? ast_term(X) : ast(X);
     return new MultiAST(kids);
   }
-  MultiAST( AST[] kids ) { super(kids); }
-  @Override void jpre(SB sb) {
-    sb.nl().ii().i();
-    if( _kids.length > 1 ) throw XEC.TODO(); // Blocks need "{}", but how exprs?
+  MultiAST( AST... kids ) { super(kids); }
+  @Override String type() {
+    String kid0 = _kids[0].type();
+    if( _kids.length==1 ) return kid0;
+    String kid1 = _kids[1].type();
+    if( kid0.equals("Boolean" ) )
+      return "?"+kid1;
+    throw XEC.TODO();
   }
-  @Override void jpost(SB sb) { sb.di(); }
+
+  @Override AST rewrite() {
+    // When finding a BinOp ":", I search the _kids[0] slot for an elvis.
+    //   ( ...e0 ?. e1 ... : alt)  ==>>
+    //   (e0==null ? alt : (... e0.e1 ...))
+    // When finding a Multi, I search the _kids[0] slot for an elvis
+    //   ( ...e0 ?. e1 ... , more)  ==>>
+    //   ((e0==null || (...e0.e1...)) && more)
+    // TODO: search all kids
+    AST elvis = _kids[0];
+    while( elvis!=null && elvis._kids!=null && !UniOpAST.is_elvis(elvis._kids[0]) )
+      elvis = elvis._kids[0];
+    if( elvis!=null && elvis._kids!=null ) {
+      // Make 'e0==null'
+      AST vsnull = elvis._kids[0]._kids[0];
+      BinOpAST eq0 = new BinOpAST("==","","boolean",new ConAST("null"),vsnull);
+      BinOpAST or = new BinOpAST("||","","boolean",eq0,_kids[0]);
+      _kids[0] = or;
+      // Drop the elvis buried inside the expression
+      elvis._kids[0] = vsnull;
+    }
+    
+    return this;
+  }
+  
+  @Override public void jpre(SB sb) {
+    if( _kids.length > 1 )
+      sb.p("(");
+  }
+  @Override public void jmid(SB sb, int i) {
+    if( _kids.length > 1 )
+      sb.p(") && (");
+  }
+  @Override public void jpost(SB sb) {
+    if( _kids.length > 1 )
+      sb.unchar(5);
+  }
 }
--- javatools/src/main/java/org/xvm/cc_explore/xclz/PropertyAST.java
+++ javatools/src/main/java/org/xvm/cc_explore/xclz/PropertyAST.java
@@ -2,12 +2,13 @@ package org.xvm.cc_explore.xclz;
 
 import org.xvm.cc_explore.cons.TCon;
 import org.xvm.cc_explore.util.SB;
+import org.xvm.cc_explore.XEC;
 
 class PropertyAST extends AST {
-  public final String _prop;
+  public String _prop, _type;
   static PropertyAST make( XClzBuilder X ) {
     AST lhs = ast_term(X);    
-    String prop = XClzBuilder.value_tcon((TCon)X.con());
+    String prop = XClzBuilder.value_tcon(X.con());
     if( lhs instanceof ConAST con && 
         con._con.equals(XClzBuilder.java_class_name(X._mod._name)) )
       lhs = null;
@@ -17,6 +18,17 @@ class PropertyAST extends AST {
   private PropertyAST( AST lhs, String prop ) {
     super(new AST[]{lhs});
     _prop=prop;
+  }
+  @Override String type() {
+    if( _type == null ) throw XEC.TODO();
+    return _type;
+  }
+
+  @Override AST rewrite() {
+    // Java strings do not have any properties, just rewrite to the java name
+    if( _prop.equals("size$get()") && _kids[0].type().equals("String") )
+      { _prop="length()"; _type="long"; }
+    return this;
   }
   
   @Override void jpost( SB sb ) {
