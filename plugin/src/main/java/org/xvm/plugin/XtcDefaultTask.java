67d3bf1b03beba745f750ecff8989289abc541fa
Marcus Lagergren <1062473+lagergren@users.noreply.github.com>
2023-11-14 17:33:52 +0100

    Consolidating inheritance hierarchy to get rid of the project delegate code in the XTC plugin, which has become a bit of a wart.

--- plugin/src/main/java/org/xvm/plugin/XtcDefaultTask.java
+++
@@ -1,8 +0,0 @@
-package org.xvm.plugin;
-
-import org.gradle.api.DefaultTask;
-
-// TODO: This is intended a common superclass to avoid the messy delegate pattern.
-//   We also put XTC launcher common logic in here, like e.g. fork, jvmArgs and such.
-public class XtcDefaultTask extends DefaultTask {
-}
