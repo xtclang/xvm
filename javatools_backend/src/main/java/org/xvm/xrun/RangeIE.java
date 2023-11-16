3e7716865cbdb171c9d3180c4bf6e0de91c760be
Cliff Click <cliffc@acm.org>
2023-11-16 10:05:24 -0800

    Yet Another Major Refactor
    Move all java support classes to the exact name and location as their XTC variants, all under org/xvm/xtc.  i.e. xrun/XStringable -> xtc/ecstasy/test/Stringable

--- javatools_backend/src/main/java/org/xvm/xrun/RangeIE.java
+++
@@ -1,2 +0,0 @@
-package org.xvm.xrun;
-public class RangeIE extends Range { public RangeIE( long lo, long hi ) { super(lo,hi,false,true); } }
--- javatools_backend/src/main/java/org/xvm/xrun/XStringable.java
+++
@@ -1,5 +0,0 @@
-package org.xvm.xrun;
-public interface XStringable {
-  default int estimateStringLength() { return 0; }
-  default Arychar appendTo(Arychar buf) { return buf.appendTo(toString()); }
-}
