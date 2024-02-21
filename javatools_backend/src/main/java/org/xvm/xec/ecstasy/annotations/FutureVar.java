7db9e582f172f1d79b322e40aa65a0ea2db86f62
Cliff Click <cliffc@acm.org>
2024-02-21 15:11:28 -0800

    Timer launches background task
    FutureVar delegates to Java CompletableFuture

--- javatools_backend/src/main/java/org/xvm/xec/ecstasy/annotations/FutureVar.java
+++ javatools_backend/src/main/java/org/xvm/xec/ecstasy/annotations/FutureVar.java
@@ -1,13 +1,25 @@
 package org.xvm.xec.ecstasy.annotations;
 
+import org.xvm.XEC;
 import org.xvm.xec.XTC;
 import org.xvm.xrun.Never;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class FutureVar<Referent extends XTC> extends XTC {
-  public FutureVar(Never n) {}
-  public FutureVar() {}
-  private Referent _ref;
-  public Referent $get() { return _ref; }
-  public void $set(Referent ref) { _ref=ref; }
+  public FutureVar(Never n) {_future=null;}
+  
+  private final CompletableFuture<Referent> _future;
+  public FutureVar() { _future = new CompletableFuture<>(); }
+  public Referent $get() throws XTC.Exception {
+    try {
+      return _future.get();
+    } catch( InterruptedException ie ) {
+      throw XEC.TODO();         // Not expecting the XTC runtime to interrupt
+    } catch( ExecutionException ee ) {
+      throw (XTC.Exception)ee.getCause(); // Need to rethrow the completed exception
+    }
+  }
+  public void $set(Referent ref) {  _future.complete(ref); }
 }
