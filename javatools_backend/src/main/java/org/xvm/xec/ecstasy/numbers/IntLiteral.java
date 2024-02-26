01034bdea7f0b99bebe0b63c494609af66eba09e
Cliff Click <cliffc@acm.org>
2024-02-26 10:22:35 -0800

    Progress on queens test

--- javatools_backend/src/main/java/org/xvm/xec/ecstasy/numbers/IntLiteral.java
+++ javatools_backend/src/main/java/org/xvm/xec/ecstasy/numbers/IntLiteral.java
@@ -17,5 +17,6 @@ public class IntLiteral extends Const {
   public IntLiteral(long x) { _s=Long.toString(x); }
 
   public static IntLiteral construct(String s) { return new IntLiteral(s); }
+  public long toInt64() { return Long.valueOf(_s); }
   @Override public String toString() { return _s; }
 }
