eb06c568930fb58ea8f1287bcbb5002a293c637b
Cliff Click <cliffc@acm.org>
2024-04-30 09:42:24 -0700

    remove -e option echo

--- javatools_backend/bin/makedepends.sh
+++ javatools_backend/bin/makedepends.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 # Automatic make-dependency discovery for X files
 # Usage: src (without the .x)  dst  (without the .d)
-echo -ne $2.d $2.xtc ":\t" > $2.d;
+echo -n $2.d $2.xtc ":	" > $2.d;
 ((test -d $2 && (/usr/bin/find $1 -name *.x | xargs echo)) >> $2.d ) || true;
