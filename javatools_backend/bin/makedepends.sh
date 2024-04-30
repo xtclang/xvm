ec5faec5a8edda735d35340cd3af90b3cd9b328e
Cliff Click <cliffc@acm.org>
2024-04-30 09:51:17 -0700

    update exec bit

--- javatools_backend/bin/makedepends.sh
+++ javatools_backend/bin/makedepends.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Automatic make-dependency discovery for X files
+# Automatic  make-dependency discovery for X files
 # Usage: src (without the .x)  dst  (without the .d)
 echo -n $2.d $2.xtc ":	" > $2.d;
 ((test -d $2 && (/usr/bin/find $1 -name *.x | xargs echo)) >> $2.d ) || true;
