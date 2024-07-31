c64e410562be9678dc032b91385b9b1c18d23f9e
Cliff Click <cliffc@acm.org>
2024-07-31 16:41:08 -0700

    conditional mixins

--- javatools_backend/src/main/java/org/xvm/xec/ecstasy/Service.java
+++ javatools_backend/src/main/java/org/xvm/xec/ecstasy/Service.java
@@ -166,11 +166,11 @@ public abstract class Service extends XTC
     sb.ip("public ");
     ClzBuilder.ret_sig(meth,sb);
     sb.p("$").p(meth._name).p("( ");
-    ClzBuilder.args(meth,sb);
+    ClzBuilder.args(meth,sb,null);
     sb.p(" ) { ");
     if( meth.xrets()!=null ) sb.p("return ");
     sb.p("$$").p(meth._name).p("(");
-    ClzBuilder.arg_names(meth,sb);
+    ClzBuilder.arg_names(meth,sb,null);
     sb.p(").xget(); }").nl();
 
 
@@ -184,9 +184,9 @@ public abstract class Service extends XTC
     //XFT<RET> $$MNAME(ARGS) { return $enqueue(new Call$MNAME(ARGS)) }
     sb.ip("public ").p(gen_ft);
     sb.p(" $$").p(meth._name).p("( ");
-    ClzBuilder.args(meth,sb);
+    ClzBuilder.args(meth,sb,null);
     sb.p(" ) { return $enqueue(new Call$").p(meth._name).p("( ");
-    ClzBuilder.arg_names(meth,sb);
+    ClzBuilder.arg_names(meth,sb,null);
     sb.p(")); }").nl();
 
     // Now the wrapper class, one per every function call.
@@ -207,7 +207,7 @@ public abstract class Service extends XTC
         sb.ifmt("private final %0 %1;\n",meth.xarg(i).clz(),meth._args[i]._name);
     // Constructor
     sb.ifmt("Call$%0( ",meth._name);
-    ClzBuilder.args(meth,sb);
+    ClzBuilder.args(meth,sb,null);
     sb.p(") {").nl().ii();
     sb.ifmt("super(\"%0.%1\");\n",java_class_name,meth._name);
     if( meth.xargs() != null )
@@ -221,7 +221,7 @@ public abstract class Service extends XTC
     sb.p(" call() { ");
     if( meth.xrets()!=null ) sb.p("return ");
     sb.p(meth._name).p("(");
-    ClzBuilder.arg_names(meth,sb);
+    ClzBuilder.arg_names(meth,sb,null);
     sb.p("); ");
     if( meth.xrets()==null ) sb.p("return null; ");
     sb.p("}\n");                // End of call
