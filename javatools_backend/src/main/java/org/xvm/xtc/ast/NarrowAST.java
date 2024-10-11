e112f2c18937feeb72a93ce2e9a2a701214b29ac
Cliff Click <cliffc@acm.org>
2024-10-11 16:45:10 -0700

    missed one

--- javatools_backend/src/main/java/org/xvm/xtc/ast/NarrowAST.java
+++ javatools_backend/src/main/java/org/xvm/xtc/ast/NarrowAST.java
@@ -1,5 +1,7 @@
 package org.xvm.xtc.ast;
 
+import org.xvm.XEC;
+import org.xvm.util.SB;
 import org.xvm.xtc.*;
 import org.xvm.xtc.cons.AccessTCon;
 import org.xvm.xtc.cons.Const;
@@ -8,17 +10,22 @@ import org.xvm.xtc.cons.Const;
 //   if( x instanceof Y ) { Y xy = (Y)x; .... }
 // This is a free up-cast after test.
 class NarrowAST extends AST {
-
+  private final XType _xt;
   static NarrowAST make( ClzBuilder X ) {
     AST[] kids = X.kids(1);     // One expr
     Const type = X.con();
-    return new NarrowAST(kids,type);
+    XType xt = XType.xtype(type,true); // Does not unbox for free
+    return new NarrowAST(kids,xt);
   }
 
-  private NarrowAST( AST[] kids, Const type ) {
+  private NarrowAST( AST[] kids, XType xt ) {
     super(kids);
-    _type = XType.xtype(type,false);
+    _xt = xt;
   }
-  @Override XType _type() { return _type; }
+  @Override XType _type() {
+    return _kids[0]._type instanceof XBase && !(_xt instanceof XBase)
+      ? _xt.unbox()          // Preserve unboxed kid, but upcast to unboxed _xt
+      : _xt;
+  }
   @Override String name() { return _kids[0].name(); }
 }
