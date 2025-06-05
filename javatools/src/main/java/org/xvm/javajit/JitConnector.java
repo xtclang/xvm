4ee234e75bf13ec9891524f24658a0f42bfd7cf5
Gene Gleyzer <gene.gleyzer@xqiz.it>
2025-06-05 17:47:30 -0400

    Implement ModuleStructure.getDigest(); fill in some TODOs

--- javatools/src/main/java/org/xvm/javajit/JitConnector.java
+++ javatools/src/main/java/org/xvm/javajit/JitConnector.java
@@ -3,17 +3,33 @@ package org.xvm.javajit;
 
 import org.xvm.api.Connector;
 import org.xvm.asm.ModuleRepository;
+import org.xvm.asm.ModuleStructure;
 
 
 public class JitConnector
         extends Connector {
     public JitConnector(ModuleRepository repo) {
-        // TODO
-        // TODO move this logic to a JitConnector?
-        Xvm        xvm = new Xvm(repo);
-        TypeSystem ts  = xvm.createLinker().addModule(module).link();
-        // TODO add error reporting
-        Container  cnt = xvm.createContainer(ts, xvm.DefaultMainInjector);
-        // TODO reflect on cnt to get the main module and find the appropriate run() (or other) method
+        super(repo);
+
+        xvm = new Xvm(repo);
     }
+
+    @Override
+    public void loadModule(String appName) {
+        ModuleStructure module = f_repository.loadModule(appName);
+        if (module == null) {
+            throw new IllegalStateException("Unable to load module \"" + appName + "\"");
+        }
+        TypeSystem ts = xvm.createLinker().addModule(module).link();
+        // TODO add error reporting
+
+        Container container = xvm.createContainer(ts, xvm.DefaultMainInjector);
+        // TODO reflect on container to get the main module and find the appropriate run() (or other) method
+
+    }
+
+    /**
+     * The XVM within which this TypeSystem exists
+     */
+    public final Xvm xvm;
 }
