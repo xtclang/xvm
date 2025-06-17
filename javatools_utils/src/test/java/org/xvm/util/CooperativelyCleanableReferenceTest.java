85900627e3443e7f5e8339bf18c6b8a725e3a64c
markandrewfalco <mark.falco@gmail.com>
2025-06-17 11:49:44 -0400

    Addressed review feedback

---
+++ javatools_utils/src/test/java/org/xvm/util/CooperativelyCleanableReferenceTest.java
@@ -0,0 +1,46 @@
+package org.xvm.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Tests for {@link CooperativelyCleanableReference}
+ */
+public class CooperativelyCleanableReferenceTest {
+    @Test
+    void shouldClean() {
+        AtomicLong cleaned = new AtomicLong();
+        while (cleaned.get() == 0) {
+            new CooperativelyCleanableReference<>(new Object(), cleaned::incrementAndGet);
+            System.gc();
+        }
+    }
+
+    @Test
+    void shouldIgnoreExceptionsWhileCleaning() {
+        AtomicLong cleaned = new AtomicLong();
+        while (cleaned.get() == 0) {
+            new CooperativelyCleanableReference<>(new Object(), () -> {
+                cleaned.incrementAndGet();
+                throw new NullPointerException();
+            });
+            System.gc();
+        }
+    }
+
+    @Test
+    void shouldIgnoreAndRestoreInterruptExceptionsWhileCleaning() {
+        AtomicLong cleaned = new AtomicLong();
+        while (cleaned.get() == 0) {
+            new CooperativelyCleanableReference<>(new Object(), () -> {
+                cleaned.incrementAndGet();
+                throw new RuntimeException(new InterruptedException());
+            });
+            System.gc();
+        }
+        assertTrue(Thread.interrupted());
+    }
+}
