2e09829828579d66fa68edfc99050e1c3131ef1f
Cameron Purdy <699204+cpurdy@users.noreply.github.com>
2025-07-10 15:15:55 -0400

    Hopefully non-breaking changes

--- javatools/src/main/java/org/xvm/javajit/intrinsic/xException.java
+++ javatools/src/main/java/org/xvm/javajit/intrinsic/xException.java
@@ -16,5 +16,22 @@ public class xException extends RuntimeException {
         $ctx = Ctx.get();
     }
 
+    public xException(String message, Throwable cause) {
+        super(message, cause);
+        $ctx = Ctx.get();
+    }
+
     public Ctx $ctx;
+
+    public static xException newReadOnly(String text, xException cause) {
+        // TODO - how to create an Ecstasy ReadOnly exception?
+        text = text == null ? ("ReadOnly: " + text) : "ReadOnly";
+        return cause == null ? new xException(text) : new xException(text, cause);
+    }
+
+    public static xException newOutOfBounds(String text, xException cause) {
+        // TODO - how to create an Ecstasy OutOfBounds exception?
+        text = text == null ? ("OutOfBounds: " + text) : "OutOfBounds";
+        return cause == null ? new xException(text) : new xException(text, cause);
+    }
 }
