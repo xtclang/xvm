3f2210fa60569cbba28af6c18b4581017a561ab7
Gene Gleyzer <gene.gleyzer@xqiz.it>
2025-09-06 10:56:38 -0400

    Refactor _Get/_Set implementations

--- javatools/src/main/java/org/xvm/asm/op/L_Set.java
+++ javatools/src/main/java/org/xvm/asm/op/L_Set.java
@@ -7,23 +7,13 @@ import java.io.IOException;
 
 import java.lang.classfile.CodeBuilder;
 
-import java.lang.constant.ClassDesc;
-import java.lang.constant.MethodTypeDesc;
-
 import org.xvm.asm.Argument;
 import org.xvm.asm.Constant;
 import org.xvm.asm.OpProperty;
 
 import org.xvm.asm.constants.PropertyConstant;
 
-import org.xvm.asm.constants.PropertyInfo;
-import org.xvm.asm.constants.TypeConstant;
 import org.xvm.javajit.BuildContext;
-import org.xvm.javajit.BuildContext.Slot;
-
-import org.xvm.javajit.Builder;
-import org.xvm.javajit.JitMethodDesc;
-import org.xvm.javajit.TypeSystem;
 
 import org.xvm.runtime.Frame;
 import org.xvm.runtime.ObjectHandle;
@@ -115,30 +105,7 @@ public class L_Set
 
     @Override
     public void build(BuildContext bctx, CodeBuilder code) {
-        TypeSystem       ts         = bctx.typeSystem;
-        PropertyConstant idProp     = (PropertyConstant) bctx.getConstant(m_nPropId);
-        TypeConstant     typeTarget = idProp.getClassIdentity().getType();
-        ClassDesc        cdTarget   = typeTarget.ensureClassDesc(ts);
-        PropertyInfo     infoProp   = typeTarget.ensureTypeInfo().getProperties().get(idProp);
-        JitMethodDesc    jmd        = infoProp.getSetterJitDesc(ts);
-        String           methodName = infoProp.getSetterId().ensureJitMethodName(ts);
-
-        MethodTypeDesc md;
-        if (jmd.isOptimized) {
-            md         = jmd.optimizedMD;
-            methodName += Builder.OPT;
-        } else {
-            md = jmd.standardMD;
-        }
-
-        bctx.loadThis(code);
-        bctx.loadCtx(code);
-        Slot valueSlot = bctx.loadArgument(code, m_nValue);
-        if (!valueSlot.isSingle()) {
-            throw new UnsupportedOperationException("Multislot L_Set");
-        }
-
-        code.invokevirtual(cdTarget, methodName, md);
+        bctx.buildSetProperty(code, bctx.loadThis(code), m_nPropId, m_nValue);
     }
 
     // ----- fields --------------------------------------------------------------------------------
