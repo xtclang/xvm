ff6fc9b1796b65b5f4bbd33673a83569851f2b35
Marcus Lagergren <1062473+lagergren@users.noreply.github.com>
2025-10-03 13:47:01 +0200

    Kotlin warning

--- build-logic/aggregator/build.gradle.kts
+++ build-logic/aggregator/build.gradle.kts
@@ -2,20 +2,9 @@ plugins {
     `kotlin-dsl`
 }
 
-val kotlinDslJavaVersion = JavaLanguageVersion.of(24)
-
 java {
     toolchain {
-        languageVersion.set(kotlinDslJavaVersion)
-    }
-}
-
-kotlin {
-    jvmToolchain {
-        languageVersion.set(kotlinDslJavaVersion)
-    }
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24)
+        languageVersion.set(JavaLanguageVersion.of(JavaVersion.current().majorVersion.toInt()))
     }
 }
 
--- build-logic/common-plugins/build.gradle.kts
+++ build-logic/common-plugins/build.gradle.kts
@@ -6,20 +6,9 @@ plugins {
     `kotlin-dsl`
 }
 
-val kotlinDslJavaVersion = JavaLanguageVersion.of(24)
-
 java {
     toolchain {
-        languageVersion.set(kotlinDslJavaVersion)
-    }
-}
-
-kotlin {
-    jvmToolchain {
-        languageVersion.set(kotlinDslJavaVersion)
-    }
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24)
+        languageVersion.set(JavaLanguageVersion.of(JavaVersion.current().majorVersion.toInt()))
     }
 }
 
