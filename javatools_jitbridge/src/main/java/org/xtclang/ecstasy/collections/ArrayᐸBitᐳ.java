4cf29861eede25c9c909bdc1c1c64089ef37a9e2
Gene Gleyzer <gene.gleyzer@xqiz.it>
2026-04-23 09:05:13 -0400

    Add doc links

--- javatools_jitbridge/src/main/java/org/xtclang/ecstasy/collections/ArrayᐸBitᐳ.java
+++ javatools_jitbridge/src/main/java/org/xtclang/ecstasy/collections/ArrayᐸBitᐳ.java
@@ -31,7 +31,7 @@ public class ArrayᐸBitᐳ
     // ----- Array API -----------------------------------------------------------------------------
 
     /**
-     * Array Constructor: construct(Int capacity = 0)
+     * @see {@link Array#$new$p}
      */
     public static ArrayᐸBitᐳ $new$p(Ctx ctx, TypeConstant type, long capacity, boolean _capacity) {
         assert !type.isImmutable();
@@ -43,6 +43,9 @@ public class ArrayᐸBitᐳ
         return array;
     }
 
+    /**
+     * @see {@link Array#$new$1$p}
+     */
     public static ArrayᐸBitᐳ $new$1$p(Ctx ctx, TypeConstant type, long size, nObj supply) {
         if (supply instanceof Bit boxed) {
             ctx.alloc(size); // REVIEW + HEADER_SIZE?
@@ -63,11 +66,17 @@ public class ArrayᐸBitᐳ
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * @see {@link Array#$new$2}
+     */
     public static ArrayᐸBitᐳ $new$2$p(Ctx ctx, TypeConstant type, Mutability mutability, Iterable elements) {
         // TODO
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * @see {@link Array#$new$3}
+     */
     public static ArrayᐸBitᐳ $new$3$p(Ctx ctx, TypeConstant type, ArrayᐸBitᐳ that) {
         // TODO
         throw new UnsupportedOperationException();
