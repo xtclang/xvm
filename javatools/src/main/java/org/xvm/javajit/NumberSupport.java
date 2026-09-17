ea5ff66377f26e3d65eb0fba87a63fb18b22d543
Marcus Lagergren <1062473+lagergren@users.noreply.github.com>
2026-09-17 13:38:53 +0200

    Indent
    (cherry picked from commit 4fa38bd193caf3f64493ea48f25df6b3b04b6c37)

--- javatools/src/main/java/org/xvm/javajit/NumberSupport.java
+++ javatools/src/main/java/org/xvm/javajit/NumberSupport.java
@@ -205,7 +205,7 @@ public interface NumberSupport
             case "I" -> {
                 boolean fUnsigned = typeTarget.getValueString().charAt(0) == 'U';
                 if (fUnsigned) {
-                    code.invokestatic(CD_Integer,"divideUnsigned", MD_UDivInt);
+                    code.invokestatic(CD_Integer, "divideUnsigned", MD_UDivInt);
                 } else {
                     code.idiv();
                 }
@@ -214,7 +214,7 @@ public interface NumberSupport
             case "J" -> {
                 boolean fUnsigned = typeTarget.getValueString().charAt(0) == 'U';
                 if (fUnsigned) {
-                    code.invokestatic(CD_Long,"divideUnsigned", md(CD_long, CD_long, CD_long));
+                    code.invokestatic(CD_Long, "divideUnsigned", md(CD_long, CD_long, CD_long));
                 } else {
                     code.ldiv();
                 }
