548855c7800468a5dfccc25448f7f107f421ffd0
Marcus Lagergren <1062473+lagergren@users.noreply.github.com>
2026-09-17 13:37:16 +0200

    Indent
    (cherry picked from commit 1416285daaaa6de1410060e162b8b11e420a1026)

--- javatools/src/main/java/org/xvm/javajit/JitTypeDesc.java
+++ javatools/src/main/java/org/xvm/javajit/JitTypeDesc.java
@@ -172,13 +172,13 @@ public class JitTypeDesc {
             String name = sansNullable.getSingleUnderlyingClass(false).getName();
             // NOTE: isJavaPrimitive() and this switch must list the same names
             return switch (name) {
-                case "Byte", "Nibble", "Int8", "UInt8" -> CD_byte;
-                case "Int16", "UInt16" -> CD_short;
+                case "Byte", "Nibble", "Int8", "UInt8"                 -> CD_byte;
+                case "Int16", "UInt16"                                 -> CD_short;
                 case "Char", "Int32", "UInt32", "Float8e4", "Float8e5" -> CD_int;
-                case "Int64", "UInt64" -> CD_long;
-                case "Float16", "Float32" -> CD_float;
-                case "Float64" -> CD_double;
-                case "Boolean", "Bit" -> CD_boolean;
+                case "Int64", "UInt64"                                 -> CD_long;
+                case "Float16", "Float32"                              -> CD_float;
+                case "Float64"                                         -> CD_double;
+                case "Boolean", "Bit"                                  -> CD_boolean;
                 default -> throw new IllegalStateException("No field carrier for: " + name);
             };
         }
