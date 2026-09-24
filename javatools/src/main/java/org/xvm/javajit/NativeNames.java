0b763a5044dce0e167cd594dfb2545ce2061da89
Gene Gleyzer <gene.gleyzer@xqiz.it>
2026-09-24 11:38:33 -0400

    Remove unnecessary native names

--- javatools/src/main/java/org/xvm/javajit/NativeNames.java
+++ javatools/src/main/java/org/xvm/javajit/NativeNames.java
@@ -51,12 +51,6 @@ public class NativeNames {
         reservedMethodName.put("numbers.Random/int8/0",    "int8");
         reservedMethodName.put("numbers.Random/uint8/0",   "uint8");
 
-        reservedMethodName.put("numbers.RTRandom/bit/0",   "bit");
-        reservedMethodName.put("numbers.RTRandom/bits/1",  "bits");
-        reservedMethodName.put("numbers.RTRandom/int/1",   "int");
-        reservedMethodName.put("numbers.RTRandom/int8/0",  "int8");
-        reservedMethodName.put("numbers.RTRandom/uint8/0", "uint8");
-
         reservedMethodName.put("collections.Array/add/1",        "add");
         reservedMethodName.put("collections.Array/addAll/1",     "addAll");
         reservedMethodName.put("collections.Array/delete/1",     "delete");
